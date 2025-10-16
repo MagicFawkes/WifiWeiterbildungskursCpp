2564fd2f8adab43999734f1713880ff8b13ee38b
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-16 23:23:00 +0200

    Add Aufgabe07.cpp and update project configuration
    Added `Aufgabe07.cpp` with initial structure for income tax calculation program. Updated `Basiskurs.vcxproj` and `Basiskurs.vcxproj.filters` to include the new file. Changed active task in `AufgabenMain.cpp` to execute `Aufgabe07.cpp`. The new file includes placeholders for input handling and tax calculation logic.

---
+++ Basiskurs/Woche01/Aufgaben/Aufgabe07.cpp
@@ -0,0 +1,20 @@
+/*
+Schreiben Sie ein C Programm, welches die Lohnsteuer eines Angestellten berechnen soll. Der
+benutzer soll sein jährliches Gehlat in Brutto eingeben und sein Netto Gehalt und die bezahlten
+Lohnsteuer erfahren.
+Das Programm soll folgendes besitzen:
+• Eine Funktion zur Speicherung der Eingabe
+• Eine Funktion zur Berechnung der Lohnsteuer. Diese Funktion ist eine void Funktion (kein
+Wert wird returniert, alle Variablen sollen in der main definiert werden)
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main()
+{
+    return 0;
+}
+
