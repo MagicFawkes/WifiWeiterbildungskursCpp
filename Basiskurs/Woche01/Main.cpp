b9fec8ac5430fa47fadee1271cf8e035eb445c82
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-16 23:30:22 +0200

    Simplify and unify project structure
    Consolidated "Woche01 Aufgaben" and "Woche01 Beispiele" into a single "Woche01" configuration to streamline the project structure. Removed redundant configurations and property groups, and updated build exclusion conditions accordingly.
    
    Replaced `AufgabenMain.cpp` and `BeispieleMain.cpp` with a unified `Main.cpp` file, centralizing conditional compilation logic for tasks and examples.
    
    Updated the solution file to reflect the new configuration structure and upgraded the Visual Studio version to `17.14.36603.0`. Adjusted the filters file to align with the new source file organization.
    
    These changes simplify the build process, improve maintainability, and ensure compatibility with the latest tools.

--- Basiskurs/Woche01/Main.cpp
+++ Basiskurs/Woche01/Main.cpp
@@ -1,5 +1,6 @@
 
-#define AKTIVE_BEISPIEL 1
+#define AKTIVE_BEISPIEL 0
+#define AKTIVE_AUFGABE 7
 
 #if AKTIVE_BEISPIEL == 1
 #include "Beispiele/AverageBerechnung.cpp"
@@ -27,4 +28,21 @@
 #include "Beispiele/QuersummeFunktion.cpp"
 #elif AKTIVE_BEISPIEL == 13
 #include "Beispiele/ZiffernVertauscher.cpp"
+#endif
+
+
+#if AKTIVE_AUFGABE == 1
+#include "Aufgaben/Aufgabe01.cpp"
+#elif AKTIVE_AUFGABE == 2
+#include "Aufgaben/Aufgabe02.cpp"
+#elif AKTIVE_AUFGABE == 3
+#include "Aufgaben/Aufgabe03.cpp"
+#elif AKTIVE_AUFGABE == 4
+#include "Aufgaben/Aufgabe04.cpp"
+#elif AKTIVE_AUFGABE == 5
+#include "Aufgaben/Aufgabe05.cpp"
+#elif AKTIVE_AUFGABE == 6
+#include "Aufgaben/Aufgabe06.cpp"
+#elif AKTIVE_AUFGABE == 7
+#include "Aufgaben/Aufgabe07.cpp"
 #endif
