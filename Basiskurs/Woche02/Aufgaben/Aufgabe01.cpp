fe698f4c7e956185b18e1834950f5d98e49d319b
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-17 21:01:47 +0200

    Add Woche02 configurations and new source files
    Introduced new project configurations for Woche02 (Win32 and x64) in Basiskurs.vcxproj and updated build exclusions for existing files. Added Woche02-specific source files (Aufgabe01.cpp, Beispiel01.cpp, Main.cpp) with appropriate filters and build settings. Updated WifiWeiterbildungskursCpp.sln to include Woche02 configurations. Modified Main.cpp to conditionally include examples and tasks. Added simple implementations for Aufgabe01 and Beispiel01.

---
+++ Basiskurs/Woche02/Aufgaben/Aufgabe01.cpp
@@ -0,0 +1,14 @@
+/*
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Aufgabe01");
+
+    return 0;
+}
---
+++ Basiskurs/Woche02/Beispiele/Beispiel01.cpp
@@ -0,0 +1,14 @@
+/*
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Beispiel01");
+
+    return 0;
+}
