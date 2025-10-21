52ab4eb6c3f45f80ae1c904f6c671c2c0791f22a
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-21 14:12:24 +0200

    Add pointer swap function and update main for demonstration
    A new function `pointerTausch` was added to swap two `int*` pointers using their addresses. The `main` function was updated to demonstrate the usage of this function by dynamically allocating memory for two integers, swapping their pointers, and printing the results before and after the swap. Memory cleanup was added to free allocated resources. Comments were included to explain the functionality and provide an example.

--- Basiskurs/Woche02/Beispiele/ZahlenTauschPointer.cpp
+++ Basiskurs/Woche02/Beispiele/ZahlenTauschPointer.cpp
@@ -1,16 +1,48 @@
 ﻿/*
-
-*/
+ * Die Funktion pointerTausch tauscht zwei Pointer vom Typ int* miteinander.
+ *
+ * Parameter:
+ *   - int** a: Adresse des ersten int-Pointers
+ *   - int** b: Adresse des zweiten int-Pointers
+ *
+ * Funktionsweise:
+ *   Da die Funktion die Adressen der Pointer (also Pointer auf Pointer) erhält,
+ *   kann sie die tatsächlichen Zeiger in main() vertauschen.
+ *   Dadurch zeigen nach dem Funktionsaufruf beide Pointer auf die jeweils
+ *   andere Speicheradresse.
+ *
+ * Beispiel:
+ *   Wenn a auf 47 und b auf 5 zeigt, zeigen nach dem Aufruf beide Pointer
+ *   auf die jeweils andere Zahl.
+ */
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdlib.h>
 #include <stdio.h>
 
+void pointerTausch(int** a, int** b);
+
 int main()
 {
-    
-	
+	int *zahl1 = (int*)malloc(sizeof(int));
+	*zahl1 = 47;
+	int *zahl2 = (int*)malloc(sizeof(int));
+	*zahl2 = 5;
+
+	printf("Vor dem Tausch: zahl1 = %d, zahl2 = %d\n", *zahl1, *zahl2);
+	pointerTausch(&zahl1,&zahl2);
+	printf("Nach dem Tausch: zahl1 = %d, zahl2 = %d\n", *zahl1, *zahl2);
+
+	free(zahl1);
+	free(zahl2);
 
     return 0;
+}
+
+void pointerTausch(int** a, int** b)
+{
+	int* temp = *a;
+	*a = *b;
+	*b = temp;
 }
