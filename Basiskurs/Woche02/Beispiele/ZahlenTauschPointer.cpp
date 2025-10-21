2138eafc85d9925e8bc027a80bfd616838c53d91
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-21 13:46:03 +0200

    Add ZahlenTauschPointer example and update project config
    Added `ZahlenTauschPointer.cpp` to the project with a basic
    structure and excluded it from builds for `Woche01|x64` and
    `Woche02|x64` configurations. Updated `Main.cpp` to activate
    the new example by setting `AKTIVE_BEISPIEL` to 3 and adding
    a conditional include for the new file.

---
+++ Basiskurs/Woche02/Beispiele/ZahlenTauschPointer.cpp
@@ -0,0 +1,16 @@
+﻿/*
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    
+	
+
+    return 0;
+}
--- Basiskurs/Woche02/Main.cpp
+++ Basiskurs/Woche02/Main.cpp
@@ -1,11 +1,13 @@
 
-#define AKTIVE_BEISPIEL 2
+#define AKTIVE_BEISPIEL 3
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
 #include "Beispiele/LedUmschaltung.cpp"
 #elif AKTIVE_BEISPIEL == 2
 #include "Beispiele/Roboterturnier.cpp"
+#elif AKTIVE_BEISPIEL == 3
+#include "Beispiele/ZahlenTauschPointer.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
