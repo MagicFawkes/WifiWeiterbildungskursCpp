9f4772b56aaaa0bed1679ad74260b5809b33b94e
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-10-28 23:13:52 +0100

    Add "Woche03" project configuration and source files
    Introduced new configurations for "Woche03" in `Basiskurs.vcxproj`
    and `WifiWeiterbildungskursCpp.sln`, including `Win32` and `x64`
    platforms. Added new source files (`Aufgabe01.cpp`, `Beispiel01.cpp`,
    and `Main.cpp`) with conditional compilation support in `Main.cpp`.
    
    Updated build exclusions, library dependencies, and filters for
    "Woche03". Ensured consistency in project structure and reused
    static library `GeometrieLibStatic.lib` from "Woche02". Added
    placeholders for Fibonacci sequence program in new source files.

---
+++ Basiskurs/Woche03/Aufgaben/Aufgabe01.cpp
@@ -0,0 +1,14 @@
+/*
+Aufgabe 4
+Erstellen Sie ein Programm zur Berechnung der Fibonacci Folge
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Test");
+    return 0;
+}
