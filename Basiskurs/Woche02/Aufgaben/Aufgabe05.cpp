6c418e6ed129739b32ebec916a95d7601360aede
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-01 11:49:49 +0100

    Add functionality to find nth largest number in array
    Introduced new functionality in `Aufgabe05.cpp` to determine
    the nth largest number in a predefined array. Added two new
    functions: `eingabe` for user input with error handling and
    `arraySortieren` for sorting the array in descending order.
    
    Updated the `main` function to:
    - Remove placeholder `printf("Test")`.
    - Define and process the array.
    - Prompt the user for input and validate it.
    - Print the nth largest number or an error message.
    
    In `Main.cpp`, updated the macro `AKTIVE_AUFGABE` from `4`
    to `5` to activate the new task.

--- Basiskurs/Woche02/Aufgaben/Aufgabe05.cpp
+++ Basiskurs/Woche02/Aufgaben/Aufgabe05.cpp
@@ -8,9 +8,62 @@ des Arrays aus. Beispiel n = 3 dritt größte Zahl.
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+int eingabe(const char* message);
+void arraySortieren(int arr[], int anzahl);
 
 int main()
 {
-    printf("Test");
+    int arr[] = { 2, 0, 1, 3, 4, 5, 3, 7, 8, 9, 10 };
+    int anzahl = sizeof(arr) / sizeof(arr[0]);
+
+	int zahl = eingabe("Geben Sie eine Zahl n ein, um die n-groesste Zahl zu finden: ");
+
+    arraySortieren(arr, anzahl);
+
+    if (zahl <= anzahl && zahl > 0)
+    {
+        printf("\n%d. groesste Zahl ist %d", zahl, arr[zahl - 1]);
+    }
+    else
+    {
+        printf("\nZahl nicht im gueltigen Bereich!\n");
+    }
+
     return 0;
+}
+
+int eingabe(const char* message)
+{
+    int wert;
+    int result;
+
+    printf("%s", message);
+    result = scanf("%d", &wert);
+
+    if (result != 1)
+    {
+        // Handle input error, e.g., set default value or print error
+        printf("Ungueltige Eingabe. Standardwert 0 wird verwendet.\n");
+        wert = 0;
+    }
+
+    return wert;
+}
+
+void arraySortieren(int arr[], int anzahl)
+{
+    int tmp = 0;
+
+    for (int i = 0; i < anzahl; i++)
+    {
+        for (int u = i + 1; u < anzahl; u++)
+        {
+            if (arr[u] > arr[i])
+            {
+                tmp = arr[i];
+                arr[i] = arr[u];
+                arr[u] = tmp;
+            }
+        }
+    }
 }
--- Basiskurs/Woche02/Main.cpp
+++ Basiskurs/Woche02/Main.cpp
@@ -1,6 +1,6 @@
 
 #define AKTIVE_BEISPIEL 0
-#define AKTIVE_AUFGABE 4
+#define AKTIVE_AUFGABE 5
 
 #if AKTIVE_BEISPIEL == 1
 #include "Beispiele/LedUmschaltung.cpp"
