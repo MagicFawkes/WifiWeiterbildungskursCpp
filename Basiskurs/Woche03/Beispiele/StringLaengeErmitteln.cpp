a4c58315765adc54b591f3c8e71e7e4f7e5cebe8
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-03 16:03:54 +0100

    Enhance dynamic string handling and update macro value
    Updated `StringLaengeErmitteln.cpp` to dynamically allocate
    memory for user input using `malloc` and `realloc`, enabling
    handling of strings with arbitrary length. Added error
    handling for memory allocation failures and implemented
    logic to calculate and print string length.
    
    Modified `Main.cpp` to change the `AKTIVE_BEISPIEL` macro
    value from `7` to `5`, affecting the conditional compilation
    of example files.

--- Basiskurs/Woche03/Beispiele/StringLaengeErmitteln.cpp
+++ Basiskurs/Woche03/Beispiele/StringLaengeErmitteln.cpp
@@ -5,10 +5,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-   
+    char* name = (char*)malloc(sizeof(char));
+    int c = 0;
+    int i = 0;
+    void* tmp;
+
+    if (name == NULL)
+    {
+        printf("Fehler bei der Speicherallokation\n");
+        return 1;
+    }
+
+    printf("Bitte geben Sie ihren Namen ein: ");
+
+    while ((c = getchar()) != '\n')
+    {
+        tmp = realloc(name, i + 2); // +1 fuer das neue Zeichen, +1 fuer das '\0'
+
+        if (tmp == NULL)
+        {
+            printf("Fehler bei der Speicherallokation\n");
+            free(name);
+            return 1;
+        }
+
+        name = (char*)tmp;
+        name[i] = (char)c;
+        i++;
+    }
+    name[i] = '\0';
+
+    printf("Stringlaenge ist: %d\n", i);
 
     return 0;
 }
