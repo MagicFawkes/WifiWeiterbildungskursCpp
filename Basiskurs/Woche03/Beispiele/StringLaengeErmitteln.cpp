b39167da05a5138f79aa568a89861a3f37534d53
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-03 14:54:54 +0100

    Simplify program by removing input validation logic
    Removed the `compare` function and all related input validation
    logic from the `main` function. The program now directly returns
    `0` without performing any user input or word comparison. Added
    `#define _CRT_SECURE_NO_WARNINGS` to suppress warnings for unsafe
    C runtime functions.

--- Basiskurs/Woche03/Beispiele/StringLaengeErmitteln.cpp
+++ Basiskurs/Woche03/Beispiele/StringLaengeErmitteln.cpp
@@ -6,53 +6,10 @@
 
 #include <stdio.h>
 
-bool compare(char eingabe[]);
-
 int main()
 {
-    char eingabe[20];
-    
-    do
-    {
-		printf("Was ist Ihre Lieblingsfrucht?: ");
-        scanf("%s", eingabe);
-
-    } while (!compare(eingabe));
-
-    printf("Wort richtig erkannt");
+   
 
     return 0;
 }
 
-bool compare(char eingabe[])
-{
-    char compare[] = "apple";
-    int i = 0;
-
-    while (eingabe[i] != '\0')
-    {
-        i++;
-    }
-
-    if (i != 5)
-    {
-        return false;
-    }
-
-    if (eingabe[0] != 'a' && eingabe[0] != 'A')
-        return false;
-
-    i = 1;
-
-    while (eingabe[i] != '\0')
-    {
-        if (eingabe[i] != compare[i])
-        {
-            return false;
-        }
-
-		i++;
-    }
-
-    return true;
-}
