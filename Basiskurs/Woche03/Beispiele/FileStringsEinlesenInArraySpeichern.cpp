6604d9c43679d2a9223e0dc4d1cd980d5250ffce
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-04 10:53:55 +0100

    Add file reading example and update project configuration
    Added `FileStringsEinlesenInArraySpeichern.cpp` to demonstrate reading strings from a file and printing them to the console. Updated `Basiskurs.vcxproj` and `Basiskurs.vcxproj.filters` to include the new file and exclude it from specific builds. Modified `Main.cpp` to activate the new example with `AKTIVE_BEISPIEL` set to 10. Updated `namenliste.txt` with sample names for testing.

---
+++ Basiskurs/Woche03/Beispiele/FileStringsEinlesenInArraySpeichern.cpp
@@ -0,0 +1,34 @@
+﻿/*
+
+Datei.txt Inhalt:
+x 8 y  45 test100
+Hallo 55 Welt -23
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	FILE* ptrRead = fopen("namenliste.txt", "r");
+
+    if (ptrRead == NULL)
+    {
+		printf("File konnte nicht geoeffnet werden.\n");
+		return 1;
+    }
+
+	char name[100];
+
+	while (fscanf(ptrRead, "%s", name) != EOF)
+	{
+		printf("Gelesener Name: %s\n", name);
+
+	}
+
+	fclose(ptrRead);
+
+    return 0;
+}
--- Basiskurs/Woche03/Main.cpp
+++ Basiskurs/Woche03/Main.cpp
@@ -1,5 +1,5 @@
 
-#define AKTIVE_BEISPIEL 9
+#define AKTIVE_BEISPIEL 10
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
@@ -20,6 +20,8 @@
 #include "Beispiele/FileZahlenEinlesenArray.cpp"
 #elif AKTIVE_BEISPIEL == 9
 #include "Beispiele/FileEinlesenUndKlonen.cpp"
+#elif AKTIVE_BEISPIEL == 10
+#include "Beispiele/FileStringsEinlesenInArraySpeichern.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
