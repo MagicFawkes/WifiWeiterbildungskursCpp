ba7765c9992ffdf3cb6e289710c32d636f930beb
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-09 12:07:56 +0100

    Implement file reading and word processing in Aufgabe02
    Updated Aufgabe02.cpp to read and process words from a file.
    Added dynamic memory allocation for storing words and ASCII
    character counting. Activated the new functionality in Main.cpp
    by setting AKTIVE_AUFGABE to 2. Added sample text to
    aufgabe02.txt for testing.

--- Basiskurs/Woche03/Aufgaben/Aufgabe02.cpp
+++ Basiskurs/Woche03/Aufgaben/Aufgabe02.cpp
@@ -12,9 +12,63 @@ Speicherallokierung).
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAXCOUNT 128
 
 int main()
 {
-    printf("Test");
-    return 0;
+	{
+		FILE* ptrRead = fopen("aufgabe02.txt", "r");
+
+		if (ptrRead == NULL)
+		{
+			printf("File konnte nicht geoeffnet werden.\n");
+			return 1;
+		}
+
+		int ascii[MAXCOUNT] = { 0 };
+		int zeilen = 0;
+
+		// Dynamisches Array von Strings (Pointer auf Pointer auf char)
+		char** namenListe = NULL;  // WICHTIG: initialisieren!
+
+		int c = 0;
+		int imWort = 0;
+		int zaehler = 0;
+
+		while ((c = fgetc(ptrRead)) != EOF)
+		{
+			ascii[c]++;
+
+			if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+			{
+				if (!imWort)
+				{
+					zaehler = 0;
+					zeilen++;
+					namenListe = (char**)realloc(namenListe, zeilen * sizeof(char*));
+					namenListe[zeilen - 1] = NULL; // WICHTIG: initialisieren!
+					imWort = 1;
+				}
+
+				namenListe[zeilen - 1] = (char*)realloc(namenListe[zeilen - 1], zaehler + 1);		// +1 fuer das Nullterminierungszeichen
+				namenListe[zeilen - 1][zaehler - 1] = (char)c; // Zeichen speichern
+				namenListe[zeilen - 1][zaehler] = '\0'; // Nullterminierung
+			}
+			else if (c == ' ')
+			{
+				imWort = 0;
+			}
+		}
+
+		printf("Text:\n");
+		for (int i = 0; i < zeilen; i++)
+		{
+			printf("%s ", namenListe[i]);
+			free(namenListe[i]);
+		}
+
+		free(namenListe);
+	}
 }
