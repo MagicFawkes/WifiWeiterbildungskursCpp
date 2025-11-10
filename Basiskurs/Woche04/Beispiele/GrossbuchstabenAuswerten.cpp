acdcbe2f70e08c76258a6f527d2d9a014babfb7a
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-10 14:17:32 +0100

    Replace Palidrom.cpp with GrossbuchstabenAuswerten.cpp
    Updated project files to replace `Palidrom.cpp` with `GrossbuchstabenAuswerten.cpp`.
    - Modified `Basiskurs.vcxproj` and `Basiskurs.vcxproj.filters` to reflect the new file.
    - Removed `Palidrom.cpp`, which contained no functional code.
    - Added `GrossbuchstabenAuswerten.cpp`, implementing a function to check for uppercase characters in a string.
    - The new file includes a user-interactive `main` function and uses standard C++ libraries.

---
+++ Basiskurs/Woche04/Beispiele/GrossbuchstabenAuswerten.cpp
@@ -0,0 +1,36 @@
+﻿/*
+ Für Strings ist eine Funktion:
+ int containsUpperCaseChar(char* str)
+ zu implementieren. Sie soll Wahr als Ergebnis liefern, wenn der übergebene String str einen Großbuchstaben enthält.
+*/
+
+#include <iostream>
+
+int containsUpperCaseChar(const char* str);
+
+int main()
+{
+	using namespace std;
+
+	char text[50];
+
+	cout << "Bitte String eingeben: ";
+	cin >> text;
+
+	cout << "Beinhaltet Grossbuchstaben: " << containsUpperCaseChar(text);
+
+	return 0;
+}
+
+int containsUpperCaseChar(const char* str)
+{
+	for (int i = 0; i < (int)strlen(str); ++i)
+	{
+		if (isupper(str[i])) 
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
