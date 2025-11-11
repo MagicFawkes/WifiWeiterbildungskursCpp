5da9bf32fe0a2947cf8473052dd6fc4ea1c9d79a
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-11 15:16:24 +0100

    Improve word censorship and add case-insensitivity
    - Added `#include <algorithm>` for `std::transform` usage.
    - Implemented case-insensitive word matching using a lowercase buffer.
    - Introduced `zensiert` variable to count censored words.
    - Expanded censored words list by adding `"behindert"`.
    - Fixed inner loop variable shadowing and improved replacement logic.
    - Added output to display the number of censored words.

--- Basiskurs/Woche04/Beispiele/WortZensierung.cpp
+++ Basiskurs/Woche04/Beispiele/WortZensierung.cpp
@@ -8,6 +8,7 @@ Es soll die Anzahl der zensierten Wörter angegeben werden.
 https://cplusplus.com/reference/string/string/find/
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -17,31 +18,37 @@ int main()
 	using namespace std;
 
 	string textnachricht;
+	int zensiert = 0;
 
 	cout << "Bitte Text eingeben:";
 	getline(cin, textnachricht);
 
-	std::string str2[] = { "scheisse", "doof", "dumm" };
+	std::string str2[] = { "scheisse", "doof", "dumm", "behindert" };
 
 	int anzahl = std::size(str2);
 
+	string buffer = textnachricht;
+	std::transform(buffer.begin(), buffer.end(), buffer.begin(), ::tolower);
+
 	for (int i = 0; i < anzahl; i++)
 	{
-		std::size_t found = textnachricht.find(str2[i]);
+		std::size_t found = buffer.find(str2[i]);
 
 		if (found != string::npos)
 		{
+			zensiert++;
+
 			int laenge = (int)str2[i].length();
 
-			for (int i = 0; i < laenge - 1; i++)
+			for (int u = 0; u < laenge - 1; u++)
 			{
-				textnachricht[i + found + 1] = '*';
+				textnachricht[u + found + 1] = '*';
 			}
 		}
-
 	}
 
 	cout << "Text: " << textnachricht;
+	cout << "\nEs wurden " << zensiert << " Woerter zensiert!";
 
 	return 0;
 }
