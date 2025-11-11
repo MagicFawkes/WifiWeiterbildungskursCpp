40567c75a968a2678a60bc5630436e400f3a6c7a
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-11 10:46:27 +0100

    Add StrukturRoboter example and update project files
    Added `StrukturRoboter.cpp` to the project, defining a `Roboter` structure with nested `greifer` and `position` structs. Updated `Basiskurs.vcxproj` and `Basiskurs.vcxproj.filters` to include the new file. Modified `Main.cpp` to activate the example when `AKTIVE_BEISPIEL` is set to 7. The program initializes and prints `Roboter` details. Placeholder comment added for future custom `realloc` implementation.

---
+++ Basiskurs/Woche04/Beispiele/StrukturRoboter.cpp
@@ -0,0 +1,34 @@
+﻿/*
+Schreibe ein c++ Programm, das eine eigenee realloc Funktion in C++ implementiert.
+*/
+
+#include <iostream>
+
+struct greifer
+{
+	int x = 10;
+	int y = 20;
+};
+
+struct position
+{
+	int x = 30;
+	int y = 40;
+};
+
+struct Roboter
+{
+	std::string name = "Test";
+	struct greifer greifer;
+	struct position position;
+};
+
+int main()
+{
+	struct Roboter roboter;
+
+	std::cout << roboter.name << '\n' << roboter.greifer.x << '\n' << roboter.greifer.y << '\n' << roboter.position.x << '\n' << roboter.position.y;
+
+	return 0;
+}
+
