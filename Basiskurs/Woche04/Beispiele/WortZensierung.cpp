0208331c238e4f1eac2b61bfd5193f24a74db497
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-11 14:46:38 +0100

    Add word censorship and improve input handling
    Replaced `cin` with `getline` to support multi-word input.
    Added functionality to censor a hardcoded forbidden word
    using `std::string::find` and replaced its characters with
    asterisks. Updated the program to output the censored text.
    Included the `<string>` header for string operations.

--- Basiskurs/Woche04/Beispiele/WortZensierung.cpp
+++ Basiskurs/Woche04/Beispiele/WortZensierung.cpp
@@ -5,9 +5,11 @@ std::string::find. Wenn das Wort nicht gefunden wird, gibt find den speziellen
 Wert std::string::npos zurück. Es soll am Anfang des Programms bestimmt werden,
 wie viele Nachrichten gesendet werden und die zensierte Konversation ausgegeben.
 Es soll die Anzahl der zensierten Wörter angegeben werden.
+https://cplusplus.com/reference/string/string/find/
 */
 
 #include <iostream>
+#include <string>
 
 
 int main()
@@ -16,8 +18,23 @@ int main()
 
 	string textnachricht;
 
-	cout << "Bitte Text eingeben: ";
-	cin >> textnachricht;
+	cout << "Bitte Text eingeben:";
+	getline(cin, textnachricht);
+
+	std::string str2("scheisse");
+
+	// different member versions of find in the same order as above:
+	std::size_t found = textnachricht.find(str2);
+
+	if (found != string::npos)
+	{
+		for (int i = 0; i < (int)str2.length() - 1; i++)
+		{
+			textnachricht[i + found+1] = '*';
+		}
+	}
+
+	cout << "Text: " << textnachricht;
 
 	return 0;
 }
