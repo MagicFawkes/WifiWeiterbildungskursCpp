ff9fc5845abe22140d947f56e699e58d2f7cd347
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-11 15:00:59 +0100

    Refactor and add linked list example code
    Modified `elementAmAnfangHinzufügen` in `DatenstrukturVerknüpfung.cpp` to initialize and link a new node.
    
    Added a large block of commented-out code implementing a complete linked list in C++, including functions for adding, removing, and printing nodes, as well as freeing memory.
    
    Included a `main` function with a menu-driven interface for demonstrating linked list operations. Commented code is marked as a course solution (`//Lösung von Kursleter`).

--- Basiskurs/Woche04/Beispiele/DatenstrukturVerknüpfung.cpp
+++ Basiskurs/Woche04/Beispiele/DatenstrukturVerknüpfung.cpp
@@ -77,3 +77,148 @@ void elementAmAnfangHinzufügen(struct example* struktur)
 	struktur->vorher = new example{ ++wert };
 	struktur->vorher->weiter = struktur;
 }
+
+//Lösung von Kursleter
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//void print();
+//void Insertb(Node** head, int value);
+//void printList(Node** head);
+//
+//struct Node {
+//	int data;
+//	Node* nextnode;
+//};
+//
+//enum werte {
+//	ADD_B = 1, ADD_E, REMOVE, PRINT, END
+//};
+//
+//
+//void printList(Node** head) {
+//	Node* temp = *head;
+//	while (temp != nullptr) {
+//		cout << temp->data << endl;
+//		temp = temp->nextnode;
+//	}
+//	cout << "nullptr" << endl;
+//}
+//void print() {
+//	cout << "Menu" << endl;
+//	cout << "1 Hinzufeugen am Anfang" << endl;
+//	cout << "2 Hinzufeugen am Ende" << endl;
+//	cout << "3 Loeschen eines Elements" << endl;
+//	cout << "4 Liste an der Konsole ausgeben" << endl;
+//	cout << "5 Programm wird beendet" << endl;
+//}
+//void Insertb(Node** head, int value)
+//{
+//	Node* newNode = new Node;
+//	newNode->data = value;
+//	newNode->nextnode = *head;
+//	*head = newNode;
+//}
+//void INsertEnde(Node** head, int value)
+//{
+//	Node* newNode = new Node;
+//	newNode->data = value;
+//	newNode->nextnode = nullptr;
+//
+//
+//	if (*head == nullptr) {
+//		newNode->nextnode = *head;
+//		*head = newNode;
+//		return;
+//	}
+//
+//	Node* temp = *head;
+//	while (temp->nextnode != nullptr) {
+//		temp = temp->nextnode;
+//	}
+//
+//	temp->nextnode = newNode;
+//
+//}
+//void remove(Node** head, int value) {
+//	if (*head == nullptr) {
+//		return;
+//	}
+//
+//	Node* temp = *head;
+//
+//	Node* temp1 = temp;
+//
+//	while (temp != nullptr && temp->data != value) {
+//		temp1 = temp;
+//		temp = temp->nextnode;
+//	}
+//
+//	if (temp != nullptr) {
+//		temp1->nextnode = temp->nextnode;
+//		delete temp;
+//	}
+//
+//
+//}
+//
+//void freelist(Node* head) {
+//	Node* temp;
+//	while (temp->nextnode != nullptr) {
+//		temp = head;
+//		head = head->nextnode;
+//		delete temp;
+//	}
+//}
+//
+//int main()
+//{
+//	Node* head = nullptr;
+//
+//	int wahl, value;
+//
+//	do {
+//		print();
+//		cin >> wahl;
+//		cout << "Wahl:" << wahl;
+//
+//		switch (wahl) {
+//		case ADD_B:
+//			cout << "Wert";
+//			cin >> value;
+//			Insertb(&head, value);
+//			break;
+//
+//		case ADD_E:
+//			cout << "Wert";
+//			cin >> value;
+//			INsertEnde(&head, value);
+//			break;
+//
+//		case REMOVE:
+//			cout << "Wert";
+//			cin >> value;
+//			remove(&head, value);
+//			break;
+//
+//		case PRINT:
+//			printList(&head);
+//			break;
+//
+//		case END:
+//			cout << "Beende das Programm";
+//			freelist(head);
+//			break;
+//
+//		default:
+//			cout << "Es wurde nichts ausgewählt";
+//			break;
+//
+//		}
+//
+//	} while (wahl != END);
+//
+//
+//
+//}
+//
