01dd95577ff38bcdeadaa03b490618dc732e78f6
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-11 10:53:11 +0100

    Add robot structure with movement and gripper methods
    Introduced a `Roboter` structure with `Bewegung` and `GreiferSetzen` methods to manage position and gripper attributes. Updated the `main` function to demonstrate these methods. Enhanced output readability with additional `std::cout` statements and comments.

--- Basiskurs/Woche04/Beispiele/StrukturRoboter.cpp
+++ Basiskurs/Woche04/Beispiele/StrukturRoboter.cpp
@@ -1,5 +1,8 @@
 ﻿/*
-Schreibe ein c++ Programm, das eine eigenee realloc Funktion in C++ implementiert.
+Schreibe ein Programm, in dem man eine Struktur für einen Roboter definiert.
+Der Roboter hat folgende Datentypen: name, greifer (struct), position (struct).
+Außerdem besitzt der Roboter folgende Fähigkeiten:
+• Bewegung und Greifer setzen (öffnen oder schließen und Greifkraft).
 */
 
 #include <iostream>
@@ -21,13 +24,28 @@ struct Roboter
 	std::string name = "Test";
 	struct greifer greifer;
 	struct position position;
+
+	void Bewegung(int x, int y)
+	{
+		position.x = x;
+		position.y = y;
+	}
+
+	void GreiferSetzen(int x, int y)
+	{
+		greifer.x = x;
+		greifer.y = y;
+	}
 };
 
 int main()
 {
 	struct Roboter roboter;
 
-	std::cout << roboter.name << '\n' << roboter.greifer.x << '\n' << roboter.greifer.y << '\n' << roboter.position.x << '\n' << roboter.position.y;
+	std::cout << "Wert:\n"<< roboter.name << '\n' << roboter.greifer.x << '\n' << roboter.greifer.y << '\n' << roboter.position.x << '\n' << roboter.position.y;
+	roboter.Bewegung(50, 60);
+	roboter.GreiferSetzen(70, 80);
+	std::cout << "\n\nWert:\n" << roboter.name << '\n' << roboter.greifer.x << '\n' << roboter.greifer.y << '\n' << roboter.position.x << '\n' << roboter.position.y;
 
 	return 0;
 }
