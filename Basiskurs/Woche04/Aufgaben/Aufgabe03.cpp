eabc67a44aa3154d812208d4d0bfbe7cda4b8d57
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-14 22:19:36 +0100

    Add student class and dynamic grade handling in Aufgabe03
    Implemented a `student` class in `Aufgabe03.cpp` with attributes for name, grades, and grade average. Added functionality to input student data, calculate grade averages, and display results. Used dynamic memory allocation for managing arrays of students and grades. Updated `Main.cpp` to activate and include `Aufgabe03.cpp` when `AKTIVE_AUFGABE` is set to `3`.

--- Basiskurs/Woche04/Aufgaben/Aufgabe03.cpp
+++ Basiskurs/Woche04/Aufgaben/Aufgabe03.cpp
@@ -5,11 +5,70 @@ Notendurchschnitt haben. Der Benutzer soll in der Lage sein, die Anzahl der Stud
 pro Studenten die Anzahl der Noten bestimmen (Dies wird nur einmal festgelegt). Am Ende des
 Programmes sollen alle Studenten mit ihren jeweiligen Namen und Noten sowie
 Notendurchschnitt sortiert ausgegeben werden
+
+Lösung mit new[] und delete[], da vector nicht erlaubt oder noch nicht gelernt
 */
 
+#include <iostream>
+#include <string>
+
+class student
+{
+public:
+	std::string namen;
+	float notendurchschnitt;
+	int *noten;
+	int anzahlNoten;
+};
 
 int main()
 {
+	int anzahlStudenten;
+	student *studenten;
+
+	std::cout << "Bitte Anzahl der Studenten eingeben: ";
+	std::cin >> anzahlStudenten;
+	
+	studenten = new student[anzahlStudenten];
+
+	for (int i = 0; i < anzahlStudenten; i++)
+	{
+		std::cout << "\nBitte Namen eingeben: ";
+		std::cin >> studenten[i].namen;
+
+		std::cout << "Bitte Anzahl der Noten eingeben: ";
+		std::cin >> studenten[i].anzahlNoten;
+
+		studenten[i].noten = new int[studenten[i].anzahlNoten];
+
+		int noteSumme = 0;
+
+		for (int u = 0; u < studenten[i].anzahlNoten; ++u)
+		{
+			std::cout << "Bitte Note eingeben: ";
+			std::cin >> studenten[i].noten[u];
+			noteSumme += studenten[i].noten[u];
+		}
+		studenten[i].notendurchschnitt = (float)noteSumme / studenten[i].anzahlNoten;
+	}
+
+	for (int i = 0; i < anzahlStudenten; i++)
+	{
+		std::cout << "\nName: ";
+		std::cout << studenten[i].namen;
+
+		std::cout << "\nAnzahl Noten: ";
+		std::cout << studenten[i].anzahlNoten;
+
+		for (int u = 0; u < studenten[i].anzahlNoten; ++u)
+		{
+			std::cout << "\nNote: ";
+			std::cout << studenten[i].noten[u];
+		}
+
+		std::cout << "\nNotendurchschnitt: ";
+		std::cout << studenten[i].notendurchschnitt;
+	}
 
     return 0;
 }
--- Basiskurs/Woche04/Main.cpp
+++ Basiskurs/Woche04/Main.cpp
@@ -1,6 +1,6 @@
 
 #define AKTIVE_BEISPIEL 0
-#define AKTIVE_AUFGABE 2
+#define AKTIVE_AUFGABE 3
 
 #if AKTIVE_BEISPIEL == 1
 #include "Beispiele/GrossbuchstabenAuswerten.cpp"
@@ -26,4 +26,8 @@
 #include "Aufgaben/Aufgabe01.cpp"
 #elif AKTIVE_AUFGABE == 2
 #include "Aufgaben/Aufgabe02.cpp"
+#elif AKTIVE_AUFGABE == 3
+#include "Aufgaben/Aufgabe03.cpp"
+#elif AKTIVE_AUFGABE == 4
+#include "Aufgaben/Aufgabe04.cpp"
 #endif
