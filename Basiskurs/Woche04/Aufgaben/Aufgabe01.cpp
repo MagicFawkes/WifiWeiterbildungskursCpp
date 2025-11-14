7eef23624b9190c424be424cb6624a7269f389ea
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-14 20:34:15 +0100

    Refactor I/O and add `testklasse` in Aufgabe01.cpp
    Refactored `Aufgabe01.cpp` to use C++-style I/O (`std::cout`)
    instead of C-style I/O (`printf`). Introduced a new class
    `testklasse` with methods for setting and retrieving private
    variables `a` and `b`. Updated `main()` to demonstrate the
    usage of `testklasse`.
    
    Removed `#define _CRT_SECURE_NO_WARNINGS` as it is no longer
    needed. Updated `Main.cpp` preprocessor directives to switch
    focus from example programs to assignment programs.

--- Basiskurs/Woche04/Aufgaben/Aufgabe01.cpp
+++ Basiskurs/Woche04/Aufgaben/Aufgabe01.cpp
@@ -8,10 +8,52 @@ this verwedent werden.
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+#include <iostream>
+
+class testklasse
+{
+public:
+
+    //Geht auch so: Initializer-List im Konstruktor bevorzugt:
+    //testklasse(int a, int b) : a(a), b(b) {}
+
+    testklasse(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    void variablen_setzen(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    int variableA_abrufen()
+    {
+        return this->a;
+    }
+
+    int variableB_abrufen()
+    {
+        return this->b;
+    }
+
+private:
+    int a;
+    int b;
+};
 
 int main()
 {
-    printf("Test");
+
+    testklasse testklasse = { 10, 20 };
+    std::cout << "Variable A: " << testklasse.variableA_abrufen() << ", Variable B: " << testklasse.variableB_abrufen() << std::endl;
+
+    testklasse.variablen_setzen(40, 50);
+
+    std::cout << "Variable A: " << testklasse.variableA_abrufen() << ", Variable B: " << testklasse.variableB_abrufen();
+
     return 0;
 }
+
