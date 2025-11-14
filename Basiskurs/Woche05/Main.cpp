92c354a3fa3475c0e9b4e34a8e340aec40f275c3
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-14 20:14:05 +0100

    Add Woche05 configurations and new source files
    Added project configurations for "Woche05" in `Basiskurs.vcxproj`
    for both `Win32` and `x64` platforms, including build settings
    and `ExcludedFromBuild` conditions for relevant files.
    
    Introduced new source files for "Woche05" (`Aufgabe01.cpp`,
    `Beispiel01.cpp`, and `Main.cpp`) and updated `Main.cpp` with
    preprocessor directives for dynamic inclusion of examples and
    tasks.
    
    Updated `Basiskurs.vcxproj.filters` to organize "Woche05" files
    and modified `WifiWeiterbildungskursCpp.sln` to include "Woche05"
    configurations for `x64` and `x86` platforms.

---
+++ Basiskurs/Woche05/Main.cpp
@@ -0,0 +1,15 @@
+
+#define AKTIVE_BEISPIEL 1
+#define AKTIVE_AUFGABE 0
+
+#if AKTIVE_BEISPIEL == 1
+#include "Beispiele/Beispiel01.cpp"
+#elif AKTIVE_BEISPIEL == 2
+#include "Beispiele/Beispiel02.cpp"
+#endif
+
+#if AKTIVE_AUFGABE == 1
+#include "Aufgaben/Aufgabe01.cpp"
+#elif AKTIVE_AUFGABE == 2
+#include "Aufgaben/Aufgabe02.cpp"
+#endif
