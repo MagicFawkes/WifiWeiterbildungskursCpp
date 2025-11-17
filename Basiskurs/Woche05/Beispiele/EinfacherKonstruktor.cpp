a0d182909d7b8cfb2f29db9c2e3983183403ef6a
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-17 10:56:40 +0100

    Add Student class with constructor and main() test
    Added a `Student` class with private attributes `name` and
    `matrikelnummer`. Implemented a constructor using an initializer
    list and added getter methods for both attributes.
    
    Introduced a `main()` function to test the `Student` class,
    demonstrating the use of uniform initialization to prevent
    narrowing conversions. Included comments explaining the
    advantages of uniform initialization.
    
    Added necessary includes and `using namespace std` for
    streamlined input/output operations.

--- Basiskurs/Woche05/Beispiele/EinfacherKonstruktor.cpp
+++ Basiskurs/Woche05/Beispiele/EinfacherKonstruktor.cpp
@@ -7,13 +7,46 @@ Aufgaben:
 Schreibe einen Konstruktor, der beide Werte setzt.
 Schreibe Getter für beide Attribute.
 Teste die Klasse im main().
+
+Narrowing bedeutet:
+Konstruktor mit () aufrufen oder mit {}
+Kurzfassung: Fast kein Unterschied im Ergebnis – nur unterschiedliche Schreibweise.
+Ein größerer oder genau passender Datentyp wird in einen kleineren oder unpassenden Datentyp konvertiert, sodass Information verloren gehen kann.
+C++ lässt solche Umwandlungen normalerweise zu – aber Uniform Initialization { } verhindert genau diese gefährlichen Fälle.
 */
 
 #include <iostream>
 
+using namespace std;
+
+class Student
+{
+private:
+	string name;
+	int matrikelnummer;
+
+public:
+	Student(string name, int matrikelnummer) : name(name), matrikelnummer(matrikelnummer) {}
+
+	string getName() const
+	{
+		return this->name;
+	}
+
+	int getMatrikelnummer() const
+	{
+		return this->matrikelnummer;
+	}
+};
+
+
 int main()
 {
+	Student student{ "Thomas", 2356 };		// {} Verhindert Narrowing
+	//Student student( "Thomas", 2356 );
 
+	cout << "Name: " << student.getName() << std::endl;
+	cout << "Matrikelnummer: " << student.getMatrikelnummer() << std::endl;
 
 	return 0;
 }
