0c0844baecb16d4e0eaaa823a03dcf7b7c0c338d
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-18 11:11:35 +0100

    Add RAII example with std::unique_ptr in UniquePointer.cpp
    Added a detailed explanation of RAII and `std::unique_ptr` in
    comments within **UniquePointer.cpp**. Introduced a `test` class
    to demonstrate the behavior of `std::unique_ptr`, including
    ownership transfer using `std::move`.
    
    Updated the `main` function in **UniquePointer.cpp** with examples
    of creating and using `std::unique_ptr`. Modified **Main.cpp** to
    activate the new example by setting `AKTIVE_BEISPIEL` to `8` and
    including the `Beispiele/UniquePointer.cpp` file.

--- Basiskurs/Woche05/Beispiele/UniquePointer.cpp
+++ Basiskurs/Woche05/Beispiele/UniquePointer.cpp
@@ -1,5 +1,12 @@
 ﻿/*
+RAII („Resource Acquisition Is Initialization“) bedeutet, dass eine Ressource genau dann erworben wird, wenn ein Objekt erzeugt wird – und automatisch wieder freigegeben wird, wenn das Objekt den Gültigkeitsbereich verlässt. Dadurch entstehen keine Speicherlecks, weil die Freigabe garantiert im Destruktor passiert.
 
+Ein std::unique_ptr ist ein RAII-Smartpointer, der alleinigen Besitz eines Objekts hat.
+Sobald der unique_ptr zerstört wird, wird automatisch auch das verwaltete Objekt gelöscht (delete).
+Dadurch muss der Entwickler niemals selbst delete aufrufen, und die Ressource ist sicher an die Lebensdauer des Smartpointers gebunden.
+
+Kurz:
+RAII sorgt dafür, dass Ressourcen automatisch verwaltet werden, und unique_ptr ist ein praktisches Werkzeug, um dynamisch erzeugte Objekte sicher zu besitzen.
 
 
 */
@@ -8,10 +15,34 @@
 
 using namespace std;
 
+class test
+{
+public:
+    void print()
+    {
+        cout << "\nPrint Methode";
+    }
+
+    test()
+    {
+        cout << "\nKonsturktor";
+    }
+
+    ~test()
+    {
+        cout << "\nDestruktor";
+    }
+};
 
 int main()
 {
+    unique_ptr<test> ptr = make_unique<test>();
 
+    // unique_ptr<test> ptr1 = ptr;     geht nicht bei unique pointer, weil unique
+    unique_ptr<test> ptr1 = move(ptr);      //Ptr übergeben mit move geht
+
+    ptr->print();
+    ptr1->print();
 
     return 0;
 }
