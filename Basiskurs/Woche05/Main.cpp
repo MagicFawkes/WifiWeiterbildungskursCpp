45373051ccf32b900dfe073e8567036c15229849
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-18 09:18:45 +0100

    Add new example: AbstrakteKlassen.cpp to Woche05
    Added `AbstrakteKlassen.cpp` to the project configuration and filters under `Woche05\Beispiele`. Updated `Main.cpp` to activate this example by setting `AKTIVE_BEISPIEL` to `6` and including the new file conditionally. Created the new file with a basic structure and an empty `main` function.

--- Basiskurs/Woche05/Main.cpp
+++ Basiskurs/Woche05/Main.cpp
@@ -1,5 +1,5 @@
 
-#define AKTIVE_BEISPIEL 5
+#define AKTIVE_BEISPIEL 6
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
@@ -12,6 +12,8 @@
 #include "Beispiele/StudentFunktion.cpp"
 #elif AKTIVE_BEISPIEL == 5
 #include "Beispiele/Reiskocher.cpp"
+#elif AKTIVE_BEISPIEL == 6
+#include "Beispiele/AbstrakteKlassen.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
