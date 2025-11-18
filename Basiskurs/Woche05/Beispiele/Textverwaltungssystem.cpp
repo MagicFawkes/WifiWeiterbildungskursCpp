82bd3605d3e465943f8c960177ed85aff088bc35
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-18 14:57:30 +0100

    Add Text class and TextKategorie enum for text management
    Introduced a `Text` class to represent text entities with attributes
    such as title, author, category, and page count. Added an `enum`
    `TextKategorie` to define text categories like `ROMAN`, `SACHBUCH`,
    and others. Implemented a constructor and getter/setter methods
    for all class attributes.

--- Basiskurs/Woche05/Beispiele/Textverwaltungssystem.cpp
+++ Basiskurs/Woche05/Beispiele/Textverwaltungssystem.cpp
@@ -6,7 +6,73 @@
 
 using namespace std;
 
+enum TextKategorie
+{
+	ROMAN,
+	SACHBUCH,
+	BIOGRAFIE,
+	KIDNERBUCH,
+	SCIENCE_FICTION,
+	FANTASY
+};
 
+class Text
+{
+private:
+	std::string titel;
+	std::string autor;
+	TextKategorie kategorie;
+	int seitenanzahl;
+
+public:
+	Text(string titel, string autor, TextKategorie kategorie, int seitenanzahl)
+	{
+		this->titel = titel;
+		this->autor = autor;
+		this->kategorie = kategorie;
+		this->seitenanzahl = seitenanzahl;
+	}
+
+	string getTitel() const
+	{
+		return this->titel;
+	}
+
+	void setTitel(string titel)
+	{
+		this->titel = titel;
+	}
+
+	string getAutor() const
+	{
+		return this->autor;
+	}
+
+	void setAutor(string titel)
+	{
+		this->autor = autor;
+	}
+
+	TextKategorie getKategorie() const
+	{
+		return this->kategorie;
+	}
+
+	void setKategorie(TextKategorie kategorie)
+	{
+		this->kategorie = kategorie;
+	}
+
+	int getSeitenanzahl() const
+	{
+		return this->seitenanzahl;
+	}
+
+	void setSeitenanzahl(int seitenanzahl)
+	{
+		this->seitenanzahl = seitenanzahl;
+	}
+};
 
 int main()
 {
