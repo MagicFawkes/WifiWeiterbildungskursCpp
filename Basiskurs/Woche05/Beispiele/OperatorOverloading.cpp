c5e3147181f2ddc805c554e59feeb171bbab38e9
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-18 14:38:25 +0100

    Add operator overloading example for complex numbers
    Added `OperatorOverloading.cpp` to the project configuration and
    filters under `Woche05\Beispiele`. Updated `Main.cpp` to activate
    the new example by setting `AKTIVE_BEISPIEL` to `11` and including
    the new file conditionally.
    
    Implemented a `complexnumber` class in `OperatorOverloading.cpp`
    with attributes for real and imaginary parts. Overloaded the `+`
    operator to enable addition of two `complexnumber` objects. Added
    a `main` function to demonstrate the functionality.

---
+++ Basiskurs/Woche05/Beispiele/OperatorOverloading.cpp
@@ -0,0 +1,31 @@
+﻿/*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class complexnumber {
+public:
+    float real;
+    float img;
+
+    complexnumber(float real, float img) {
+        this->real = real;
+        this->img = img;
+    }
+
+    complexnumber operator+(const complexnumber& c) const {
+        return complexnumber(real + c.real, img + c.img);
+    }
+};
+
+int main() {
+    complexnumber z1(1.0f, 1.0f);
+    complexnumber z2(0.0f, 1.0f);
+
+    complexnumber z3 = z1 + z2;
+
+    cout << z3.real << endl; // Ausgabe: 1
+    return 0;
+}
--- Basiskurs/Woche05/Main.cpp
+++ Basiskurs/Woche05/Main.cpp
@@ -1,5 +1,5 @@
 
-#define AKTIVE_BEISPIEL 10
+#define AKTIVE_BEISPIEL 11
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
@@ -22,6 +22,8 @@
 #include "Beispiele/StaticMethoden.cpp"
 #elif AKTIVE_BEISPIEL == 10
 #include "Beispiele/BibliothekVerwaltung.cpp"
+#elif AKTIVE_BEISPIEL == 11
+#include "Beispiele/OperatorOverloading.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
