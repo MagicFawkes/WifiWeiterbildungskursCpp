8ff455abcf0febeba77911efcf3437d0f5f6b4f9
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-23 16:45:34 +0100

    Add card shuffling functionality to Deck class
    Introduced a `mischen()` method in the `Deck` class to shuffle
    cards using random number generation with `<random>`. Updated
    the `main()` function to call `mischen()` and demonstrate the
    shuffle by printing the deck before and after. Managed card
    ownership during shuffling with `std::unique_ptr`.

--- Basiskurs/Woche05/Aufgaben/AufgabeUno.cpp
+++ Basiskurs/Woche05/Aufgaben/AufgabeUno.cpp
@@ -31,6 +31,7 @@ den Kartenstapel ausgibt
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <random>
 
 enum Color
 {
@@ -142,6 +143,26 @@ public:
 	    }
         
     }
+
+    void mischen()
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());                       
+        int randomNumber;
+
+        for (int i = 0; i < (int)deck.size(); ++i)
+        {
+            for (int u = 0; u < (int)deck[i]->karten.size(); ++u)
+            {
+                std::uniform_int_distribution<> dist(0, (int)deck[i]->karten.size() - 1);
+                randomNumber = dist(gen);
+
+                std::unique_ptr<Base> karte = std::move(deck[i]->karten[u]);
+                deck[i]->karten[u] = std::move(deck[i]->karten[randomNumber]);
+                deck[i]->karten[randomNumber] = std::move(karte);
+            }
+        }
+    }
 };
 
 int main()
@@ -163,6 +184,7 @@ int main()
     Game game = std::move(kartenDeck);
  
     game.print();
+    game.mischen();
     game.print();
 
     return 0;
