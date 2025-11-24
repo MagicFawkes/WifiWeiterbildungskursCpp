85cb562afc0670ef591de27ecf09cf6f122c0180
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-24 14:46:52 +0100

    Add Woche07 configurations and linked list examples
    Added new project configurations for "Woche07" in `Basiskurs.vcxproj` for both `Win32` and `x64` platforms, including build settings, property groups, and import groups. Updated `Basiskurs.vcxproj` to include new source files for "Woche07" and added `ExcludedFromBuild` conditions for existing files.
    
    Updated `Basiskurs.vcxproj.filters` to include filters for "Woche07" source files. Modified `WifiWeiterbildungskursCpp.sln` to include "Woche07" configurations for `x64` and `x86`.
    
    Created `Aufgabe01.cpp` with a simple `main` function. Added `VerketteteListenCPPStyle.cpp` and `VerketteteListenCStyle.cpp` implementing linked lists in C++ and C styles, respectively. Updated `Main.cpp` with preprocessor directives for selecting examples and tasks.

---
+++ Basiskurs/Woche07/Main.cpp
@@ -0,0 +1,16 @@
+
+#define AKTIVE_BEISPIEL 2
+#define AKTIVE_AUFGABE 0
+
+#if AKTIVE_BEISPIEL == 1
+#include "Beispiele/VerketteteListenCStyle.cpp"
+#elif AKTIVE_BEISPIEL == 2
+#include "Beispiele/VerketteteListenCppStyle.cpp"
+#elif AKTIVE_BEISPIEL == 3
+#endif
+
+#if AKTIVE_AUFGABE == 1
+#include "Aufgaben/Aufgabe01.cpp"
+#elif AKTIVE_AUFGABE == 2
+#include "Aufgaben/Aufgabe02.cpp"
+#endif
