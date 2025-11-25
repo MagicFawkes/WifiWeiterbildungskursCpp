eb89e39928654a04534d75f7e44bba5db1a399af
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-25 13:12:20 +0100

    Implement custom DruckerQueue for print job management
    Replaced the standard queue with a custom `DruckerQueue` class to manage print jobs. Added an `enum` (`Farbe`) to represent print job colors and a new `Aufgabe` class to encapsulate job data. The `DruckerQueue` class includes methods for enqueueing, dequeueing, and displaying jobs. Updated the `main` function to demonstrate the new queue functionality.

--- Basiskurs/Woche06/Beispiele/QueueDrucker.cpp
+++ Basiskurs/Woche06/Beispiele/QueueDrucker.cpp
@@ -4,11 +4,94 @@ STACK → LIFO
 */
 
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
+enum Farbe
+{
+    COLOR,
+    SW,
+};
+
+class Aufgabe
+{
+public:
+    string data;
+    Farbe color;
+    std::shared_ptr<Aufgabe> next;
+
+    Aufgabe(string value, Farbe color) : data(value), color(color), next(nullptr)
+    {
+    }
+};
+
+class DruckerQueue
+{
+private:
+    std::shared_ptr<Aufgabe> head = nullptr;   // erstes Element (front)
+    std::shared_ptr<Aufgabe> tail = nullptr;   // letztes Element (rear)
+    std::size_t count = 0;                  // optional: Anzahl der Elemente
+
+public:
+    void enqueue(string data, Farbe color)
+    {
+        std::shared_ptr<Aufgabe> newNode = std::make_shared<Aufgabe>(data, color);
+        newNode->next = nullptr; //optional wird beim Initialiseren bereits null gesetzt
+
+        count++;
+
+        if (head == nullptr && tail == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    std::shared_ptr<Aufgabe> dequeue()
+    {
+        std::shared_ptr<Aufgabe> tmp = head;
+
+        if (tmp == nullptr)
+        {
+            std::cout << "Empty queue" << endl;
+            return 0;
+        }
+        count--;
+
+        head = head->next;
+
+        return tmp;
+    }
+
+    void display()
+    {
+        if (head == nullptr && tail == nullptr)
+        {
+            std::cout << "Empty queue" << endl;
+            return;
+        }
+
+        std::shared_ptr<Aufgabe> tmp = head;
+
+        while (tmp != nullptr)
+        {
+            std::cout << "Daten wert: " << tmp->data << endl;
+            std::cout << "Farbe wert: " << tmp->color << endl;
+            tmp = tmp->next;
+        }
+    }
+};
+
+
 int main()
 {
- 
+    DruckerQueue queue;
+    queue.enqueue("Text1", SW);
+    queue.enqueue("Text2", COLOR);
+    queue.display();
 }
