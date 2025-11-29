05b2a0316ac264821e9c8996e65c54d2e482067f
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-29 16:49:44 +0100

    Implement doubly linked list with rotation feature
    Removed `_CRT_SECURE_NO_WARNINGS` and added necessary headers.
    Introduced `Node` class to represent doubly linked list nodes.
    Implemented `Liste` class with methods for insertion, deletion,
    and printing of list elements. Added `rotate` function to cyclically
    shift the list to the right by a specified number of elements.
    
    Updated `main` in `Aufgabe03.cpp` to demonstrate the new list
    functionality, including insertion, rotation, and printing.
    Replaced the original `printf` test with the new implementation.
    
    Modified `Main.cpp` to activate the new implementation by updating
    `#define AKTIVE_AUFGABE` from `2` to `3`.

--- Basiskurs/Woche06/Aufgaben/Aufgabe03.cpp
+++ Basiskurs/Woche06/Aufgaben/Aufgabe03.cpp
@@ -4,12 +4,185 @@ Doubly Linked List rotieren Schreibe eine Funktion: rotate(k) → verschiebt die
 nach rechts um k Elemente.
 */
 
-#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ostream>
 
-#include <stdio.h>
+class Node
+{
+public:
+	int data;
+	struct Node* next;
+	struct Node* prev;
+
+	Node(int value) : data(value)
+	{
+
+	}
+};
+
+class Liste
+{
+private:
+	Node* GetNewNode(int x)
+	{
+		Node* newNode = new Node(x);
+		newNode->data = x;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+		this->counter++;
+		return newNode;
+	}
+
+public:
+	int counter = 0;
+
+	Node* head = NULL;
+
+	void insert(int value)
+	{
+		if (this->head == NULL)
+		{
+			std::cout << "Liste ist leer, anlegen" << std::endl;
+			this->head = GetNewNode(value);
+			return;
+		}
+
+		Node* temp = this->head;
+
+		while (temp->next != NULL && temp->data < value)
+		{
+			temp = temp->next;
+		}
+
+		if (temp->data == value)
+		{
+			std::string eingabe;
+			std::cout << "Duplikat gefunden: " << value << " - einfuegen: ";
+			eingabe = "ja";
+			std::cout << eingabe << std::endl;
+
+			if (eingabe == "nein")
+				return;
+
+			Node* newNode = this->GetNewNode(value);
+			newNode->next = temp->next;
+			temp->next = newNode;
+			newNode->prev = temp;
+		}
+		else if (temp->data > value)
+		{
+			Node* newNode = this->GetNewNode(value);
+			newNode->next = temp;
+			newNode->prev = temp->prev;
+
+			if (temp->prev != nullptr)
+				temp->prev->next = newNode;
+
+			temp->prev = newNode;
+
+			if (temp == this->head)
+				this->head = newNode;
+		}
+		else
+		{
+			Node* newNode = this->GetNewNode(value);
+			newNode->next = temp->next;
+			temp->next = newNode;
+			newNode->prev = temp;
+		}
+	}
+
+	void printlist()
+	{
+		if (head == NULL) {
+			std::cout << "Liste leer" << std::endl;
+			return;
+		}
+
+		Node* temp = head;
+
+		while (temp != NULL)
+		{
+			std::cout << "Wert: " << temp->data << std::endl;
+			temp = temp->next;
+		}
+		std::cout << "\n";
+	}
+
+	void remove(int value)
+	{
+		if (head == NULL)
+		{
+			std::cout << "Liste leer" << std::endl;
+		}
+
+		Node* temp = head;
+
+		while (temp->next != NULL && temp->next->data != value) {}
+		{
+			temp = temp->next;
+		}
+		if (temp->next == NULL) {
+			std::cout << "Element nicht gefunden" << std::endl;
+		}
+
+		Node* temp2 = temp->next;
+		temp->next = temp2->next;
+		free(temp2);
+	}
+};
+
+void rotate(Liste& list, int verschiebung)
+{
+	if (list.counter < verschiebung)
+	{
+		std::cout << "Verschiebung nicht moeglich - Liste zu klein\n";
+		return;
+	}
+
+	Node* temp = list.head;
+	Node* temp1 = list.head;
+
+	for (int i = 0; i < verschiebung; ++i)
+	{
+		temp1 = temp1->next;
+	}
+
+	list.head = temp1;
+
+	temp1->prev->next = nullptr;
+
+	while (temp1->next != nullptr)
+	{
+		temp1 = temp1->next;
+	}
+	temp1->next = temp;
+}
 
 int main()
 {
-    printf("Test");
-    return 0;
+	Liste list1;
+	list1.insert(54);
+	list1.insert(5);
+	list1.insert(10);
+	list1.insert(15);
+	list1.insert(18);
+	list1.insert(8);
+	list1.insert(4);
+	list1.insert(4);
+	list1.insert(3);
+	list1.insert(2);
+	list1.insert(30);
+	list1.insert(18);
+	list1.insert(30);
+	list1.insert(18);
+	std::cout << "\n";
+	std::cout << list1.counter << std::endl;
+	list1.printlist();
+
+	rotate(list1, 20);
+	std::cout << list1.counter << std::endl;
+	list1.printlist();
+
+	return 0;
 }
