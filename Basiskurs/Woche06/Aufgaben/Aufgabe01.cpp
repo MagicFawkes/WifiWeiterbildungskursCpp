b2b0d23bd9396fbf0cb0c0db3293b47cb58e4c3f
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-11-29 11:06:27 +0100

    Add doubly linked list implementation and usage
    Introduced a `Node` class to represent nodes in a doubly linked list and a `Liste` class to manage the list. The `Liste` class includes methods for inserting values in sorted order (with duplicate handling) and printing the list. Updated the `main` function to demonstrate the usage of the `Liste` class.

--- Basiskurs/Woche06/Aufgaben/Aufgabe01.cpp
+++ Basiskurs/Woche06/Aufgaben/Aufgabe01.cpp
@@ -7,10 +7,98 @@ zusammenführen.
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <iostream>
+#include <ostream>
 #include <stdio.h>
+
+class Node
+{
+public:
+	int data;
+	struct Node* next;
+	struct Node* prev;
+
+	Node(int value) : data(value)
+	{
+
+	}
+};
+
+class Liste
+{
+private:
+	Node* GetNewNode(int x)
+	{
+		Node* newNode = new Node(x);
+		newNode->data = x;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+		this->counter++;
+		return newNode;
+	}
+
+	Node* head = NULL;
+
+public:
+	int counter = 0;
+
+	void insert(int value)
+	{
+		if (this->head == NULL) {
+			std::cout << "Liste ist leer" << std::endl;
+			this->head = GetNewNode(value);
+			return;
+		}
+
+		Node* temp = this->head;
+
+		while (temp->next != NULL && temp->data <= value)
+		{
+			temp = temp->next;
+		}
+
+		if (temp->data == value)
+		{
+			std::string eingabe;
+			std::cout << "Duplikat gefunden einfuegen?" << std::endl;
+			std::cin >> eingabe;
+
+			if (eingabe == "nein")
+				return;
+		}
+
+		Node* newNode = this->GetNewNode(value);
+		temp->next = newNode;
+		newNode->prev = temp;
+	}
+
+	void printlist() 
+	{
+		if (head == NULL) {
+			std::cout << "Liste leer" << std::endl;
+			return;
+		}
+
+		Node* temp = head;
+
+		while (temp != NULL)
+		{
+			std::cout << "Wert: " << temp->data << std::endl;
+			temp = temp->next;
+		}
+		std::cout << "\n";
+	}
+};
 
 int main()
 {
-    printf("Test");
+	Liste list;
+
+	list.insert(5);
+	list.insert(6);
+	list.insert(6);
+	list.insert(2);
+	list.printlist();
+
     return 0;
 }
