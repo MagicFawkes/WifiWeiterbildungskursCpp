50d19a2ba193c25412a3d627ac5651d2b159d776
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-01 12:35:08 +0100

    Refactor: Replace linked list with TestClass example
    The linked list implementation has been removed, including the `Node` structure and all associated functions (e.g., insertion, removal, search, and memory management).
    
    Introduced a new `TestClass` to demonstrate operator overloading:
    - Added prefix `++` and `--` operators to modify the `i` member.
    - Defined a postfix `++` operator (commented out and unused).
    
    Updated the `main` function to showcase the new `TestClass`:
    - Demonstrated usage of the overloaded operators.
    - Printed the value of `i` after applying the operators.
    
    Changed the `#define AKTIVE_BEISPIEL` macro in `Main.cpp` from `3` to `4` to activate the new example.

--- Basiskurs/Woche07/Beispiele/PostfixPräfixÜberladung04.cpp
+++ Basiskurs/Woche07/Beispiele/PostfixPräfixÜberladung04.cpp
@@ -1,192 +1,40 @@
 ﻿/*
-
+Postfix und präfix zu überladen
 */
 
 #include <iostream>
 
 using namespace std;
 
-//int counter() {
-//	static int counter = 0;
-//	counter++;
-//	return counter;
-//}
-
-struct Node
+class TestClass
 {
-	int data;
-	Node* next;
-	//static int counter;
+	//friend TestClass operator++(TestClass& obj;
+	//Merksatz --> friend ist nur notwendig, wenn eine freie Funktion auf private / protected Member zugreifen muss.
+
+public:
+	int i = 10;
 };
 
-//int Node::counter = 0;
-
-Node* getNewnode(int x) {
-	Node* newNode = new Node();
-	newNode->data = x;
-	newNode->next = NULL;
-	return newNode;
-}
-
-Node* insertFront(Node* head, int value)
+//mit Präfix ist kein int notwendig bzw. darf nicht angebeen werden void operator++(TestClass& obj, int)
+void operator++(TestClass& obj)		//kein const da das Objekt selbst verändert werden soll/darf
 {
-	Node* newNode = getNewnode(value);
-	newNode->next = head;
-	return newNode;
+	obj.i = obj.i + 1;
 }
 
-Node* InsertBack(Node* head, int value)
+//mit Präfix ist kein int notwendig bzw. darf nicht angebeen werden void operator++(TestClass& obj, int)
+void operator--(TestClass& obj)	//kein const da das Objekt selbst verändert werden soll/darf
 {
-	Node* newNode = getNewnode(value);
-	newNode->next = head;
-
-	if (head == NULL) {
-		return newNode;
-	}
-
-	Node* temp = head;
-
-	while (temp->next != NULL) // nicht temp != NULL ist falsch
-	{
-		temp = temp->next;
-	}
-
-	temp->next = newNode;
-	newNode->next = NULL;
-
-	return head;
-
+	obj.i = obj.i - 1;
 }
-
-Node* insert(Node* head, int value, int target)
-{
-	if (head == NULL) {
-		cout << "Liste ist leer" << endl;
-		return head;
-	}
-
-
-	Node* temp = head;
-
-	while (temp != NULL && temp->data != target) // w und w = w
-	{
-		temp = temp->next;
-	}
-
-	if (temp == NULL)
-	{
-		cout << "Target wurde nicht gefunden" << endl;
-	}
-
-	Node* newNode = getNewnode(value);
-	newNode->next = temp->next;
-	temp->next = newNode;
-	return head;
-
-}
-
-bool search(Node* head, int value) {
-	Node* temp = head;
-
-	while (temp != NULL) {
-		if (temp->data == value) {
-			return 1;
-		}
-		temp = temp->next;
-	}
-	return 0;
-}
-
-
-Node* remove(Node* head, int value)
-{
-	if (head == NULL) {
-		cout << "Liste leer" << endl;
-		return head;
-	}
-
-	Node* temp = head;
-
-	while (temp->next != NULL && temp->next->data != value) {}
-	{
-		temp = temp->next;
-	}
-	if (temp->next == NULL) {
-		cout << "Element nicht gefunden" << endl;
-	}
-
-	Node* temp2 = temp->next;
-	temp->next = temp2->next;
-	free(temp2);
-
-	return head;
-
-}
-
-void printlist(Node* head) {
-	if (head == NULL) {
-		cout << "Liste leer" << endl;
-		return;
-	}
-
-	Node* temp = head;
-
-	while (temp != NULL)
-	{
-		cout << temp->data << " -> ";
-		temp = temp->next;
-	}
-	cout << "\n";
-
-}
-
-void deleteList(Node* head)
-{
-	Node* temp1 = head;
-	Node* temp2 = NULL;
-	while (temp1 != NULL)
-	{
-		temp2 = temp1->next;
-		delete temp1;
-		temp1 = temp2;
-	}
-}
-
-
-//void deleteList(Node* head) 
-// {
-//	Node* temp1 = NULL;
-//
-//
-//	while (head != NULL)
-//	{
-//		temp1 = head;
-//		head = head->next;
-//		delete temp1;
-//
-//
-//	}
-//
-//}
-
-
-//void insertFront(Node** head, int value) 
-//{
-//		Node* newNode = getNewnode(value);
-//		newNode->next = *head;
-//		*head = newNode;
-//		
-//
-//}
 
 int main()
 {
-	Node* head = NULL;
-	for (int i = 0; i < 10; i++) {
-		head = insertFront(head, i + 1);
-	}
-
-	printlist(head);
-
-	deleteList(head);
+	TestClass t;
+	++t;
+	++t;
+	++t;
+	std::cout << t.i << std::endl;
+	--t;
+	--t;
+	std::cout << t.i << std::endl;
 }
