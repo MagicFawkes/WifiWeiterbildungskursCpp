1899396301cfe1fff34a5c4fca0283aa986a7719
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-01 12:38:59 +0100

    Add postfix operator example and update project config
    Added `PostfixPräfixÜberladung05.cpp` to the project under `Woche07\Beispiele` and updated `Basiskurs.vcxproj` to include it, excluding builds for all configurations except `Woche07|x64`. Updated `Basiskurs.vcxproj.filters` to reflect the new file.
    
    Modified `Main.cpp` to activate the new example by setting `AKTIVE_BEISPIEL` to `5` and including the new source file conditionally.
    
    Implemented a `TestClass` in `PostfixPräfixÜberladung05.cpp` with postfix increment (`operator++`) and decrement (`operator--`) operators. Added a `main` function to demonstrate their usage.

---
+++ Basiskurs/Woche07/Beispiele/PostfixPräfixÜberladung05.cpp
@@ -0,0 +1,40 @@
+﻿/*
+Postfix und präfix zu überladen
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class TestClass
+{
+	//friend TestClass operator++(TestClass& obj;
+	//Merksatz --> friend ist nur notwendig, wenn eine freie Funktion auf private / protected Member zugreifen muss.
+
+public:
+	int i = 10;
+};
+
+//mit Postfix ist ein int notwendig bzw. muss angebeen werden void operator++(TestClass& obj, int)
+void operator++(TestClass& obj, int)		//kein const da das Objekt selbst verändert werden soll/darf
+{
+	obj.i = obj.i + 1;
+}
+
+//mit Postfix ist ein int notwendig bzw. muss angebeen werden void operator++(TestClass& obj, int)
+void operator--(TestClass& obj, int)	//kein const da das Objekt selbst verändert werden soll/darf
+{
+	obj.i = obj.i - 1;
+}
+
+int main()
+{
+	TestClass t;
+	t++;
+	t++;
+	t++;
+	std::cout << t.i << std::endl;
+	t--;
+	t--;
+	std::cout << t.i << std::endl;
+}
--- Basiskurs/Woche07/Main.cpp
+++ Basiskurs/Woche07/Main.cpp
@@ -1,5 +1,5 @@
 
-#define AKTIVE_BEISPIEL 4
+#define AKTIVE_BEISPIEL 5
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
@@ -10,6 +10,8 @@
 #include "Beispiele/PostfixPr‰fix‹berladung03.cpp"
 #elif AKTIVE_BEISPIEL == 4
 #include "Beispiele/PostfixPr‰fix‹berladung04.cpp"
+#elif AKTIVE_BEISPIEL == 5
+#include "Beispiele/PostfixPr‰fix‹berladung05.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
