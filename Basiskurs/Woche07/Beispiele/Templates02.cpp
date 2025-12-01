62bd42c5b452ab170e4be47daa886d6c4173101f
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-01 14:06:18 +0100

    Add Complex class with operator overloading
    Implemented a `Complex` class to represent complex numbers with real and imaginary parts. Added operator overloading for `+`, `-`, and `*` to perform arithmetic operations on `Complex` objects. Introduced a `printValues` function to display the values of `Complex` objects. Updated the `main` function to demonstrate the functionality of the `Complex` class and its operators.

--- Basiskurs/Woche07/Beispiele/Templates02.cpp
+++ Basiskurs/Woche07/Beispiele/Templates02.cpp
@@ -5,11 +5,55 @@ Postfix und präfix zu überladen
 #include <iostream>
 using namespace std;
 
+class Complex
+{
+	friend Complex operator+(const Complex& obj1, const Complex& obj2);
+	friend Complex operator-(const Complex& obj1, const Complex& obj2);
+	friend Complex operator*(const Complex& obj1, const Complex& obj2);
+	friend void printValues(const Complex complex);
+	float re;
+	float im;
+	
+public:
+	Complex(float re = 0.0f, float im = 0.0f) : re(re), im(im)
+	{
+		
+	}
+};
 
+Complex operator+(const Complex& obj1, const Complex& obj2)
+{
+	return Complex(obj1.re + obj2.re, obj1.im + obj2.im);
+}
+
+Complex operator-(const Complex& obj1, const Complex& obj2)
+{
+	return Complex(obj1.re - obj2.re, obj1.im - obj2.im);
+}
+
+Complex operator*(const Complex& obj1, const Complex& obj2)
+{
+	return Complex(obj1.re * obj2.re, obj1.im * obj2.im);
+}
+
+void printValues(const Complex complex)
+{
+	std::cout << "Realteil: " << complex.re << ", Imaginaer: " << complex.im << std::endl;
+}
 
 int main()
 {
+	Complex complex1(10, 20);
+	Complex complex2(30, 50);
 
+	Complex complex3 = complex1 + complex2;
+	printValues(complex3);
+
+	complex3 = complex1 * complex2;
+	printValues(complex3);
+
+	complex3 = complex1 - complex2;
+	printValues(complex3);
 
     return 0;
 }
