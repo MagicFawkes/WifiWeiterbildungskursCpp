9f5780413e9b612e6ea9f48fbf1b381292c3d99d
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-01 15:08:55 +0100

    Refactor ComplexNumber and add ComplexNum template
    Refactored the `ComplexNumber` class by activating the `friend`
    declaration for `operator+` and making `real` and `img` private.
    Introduced a new `ComplexNum` class template with private members
    `Real` and `Image`, along with `friend` declarations for `operator+`
    and `operator-`. Added constructors, a `Display` method, and
    implementations for `operator+` and `operator-`. Note: the
    `operator-` implementation is incomplete.

--- Basiskurs/Woche07/Beispiele/Templates05.cpp
+++ Basiskurs/Woche07/Beispiele/Templates05.cpp
@@ -15,9 +15,9 @@ using namespace std;
 template<typename T>
 class ComplexNumber
 {
-    //friend ComplexNumber<T> operator+<>(const ComplexNumber<T> z1, const ComplexNumber<T> z2);
+    friend ComplexNumber<T> operator+<>(const ComplexNumber<T> z1, const ComplexNumber<T> z2);
 
-public:
+private:
     T real;
     T img;
 
@@ -54,3 +54,48 @@ int main()
 
     z3.print();
 }
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class ComplexNum;
+
+template<typename T>
+ComplexNum<T> operator-(const ComplexNum<T>& A, const ComplexNum<T>& B);
+
+template<typename T>
+ComplexNum<T> operator+(const ComplexNum<T>& A, const ComplexNum<T>& B);
+
+template<typename T>
+class ComplexNum {
+    T Image;
+    T Real;
+
+    friend ComplexNum<T> operator+<>(const ComplexNum<T>& A, const ComplexNum<T>& B);
+    friend ComplexNum<T> operator-<>(const ComplexNum<T>& A, const ComplexNum<T>& B);
+
+public:
+    ComplexNum(T R = 0, T I = 0) : Real(R), Image(I) {}
+    ~ComplexNum() = default;
+
+    void Display() const {
+        cout << "Real: " << Real << "\n"
+            << "Imaginär: " << Image << "\n\n";
+    }
+};
+
+template<typename T>
+ComplexNum<T> operator+(const ComplexNum<T>& A, const ComplexNum<T>& B) {
+    ComplexNum<T> ob(A.Real + B.Real, A.Image + B.Image);
+    return ob;
+}
+
+template<typename T>
+ComplexNum<T> operator-(const ComplexNum<T>& A, const ComplexNum<T>& B) {
+    ComplexNum<T> ob(A.Real - B.Rea
+
+*/
