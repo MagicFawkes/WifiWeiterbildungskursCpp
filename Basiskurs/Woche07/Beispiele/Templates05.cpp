e5ec32ad74b21191c3de3a9952eb3cd936e36b9f
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-01 14:44:46 +0100

    Add ComplexNumber template and integrate new example
    Added `Templates05.cpp` to implement a `ComplexNumber` class
    template with an overloaded addition operator and a `main`
    function for demonstration. Updated `Basiskurs.vcxproj` and
    `Basiskurs.vcxproj.filters` to include the new file under
    `Woche07\Beispiele`. Modified `Main.cpp` to activate the new
    example by setting `AKTIVE_BEISPIEL` to `11` and conditionally
    including `Templates05.cpp`. The `ComplexNumber` class includes
    a constructor, a `print` method, and a friend function for the
    `+` operator.

---
+++ Basiskurs/Woche07/Beispiele/Templates05.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class ComplexNumber
+{
+    friend ComplexNumber<T> operator+<>(const ComplexNumber<T> z1, const ComplexNumber<T> z2);
+
+private:
+    T real;
+    T img;
+
+public:
+    ComplexNumber(T real, T img) : real(real), img(img)
+    {
+    }
+
+    void print()
+    {
+        cout << real << endl;
+        cout << img << endl;
+    }
+};
+
+template<typename T>
+ComplexNumber<T> operator+(const ComplexNumber<T> z1, const ComplexNumber<T> z2)
+{
+    return ComplexNumber<T>(z1.real + z2.real, z1.img + z2.img);
+}
+
+int main()
+{
+    ComplexNumber<float> z1(1, 2);
+    ComplexNumber<float> z2(3, 2);
+    ComplexNumber<float> z3 = z1 + z2;
+
+    z3.print();
+}
--- Basiskurs/Woche07/Main.cpp
+++ Basiskurs/Woche07/Main.cpp
@@ -1,5 +1,5 @@
 
-#define AKTIVE_BEISPIEL 10
+#define AKTIVE_BEISPIEL 11
 #define AKTIVE_AUFGABE 0
 
 #if AKTIVE_BEISPIEL == 1
@@ -22,6 +22,8 @@
 #include "Beispiele/Templates03.cpp"
 #elif AKTIVE_BEISPIEL == 10
 #include "Beispiele/Templates04.cpp"
+#elif AKTIVE_BEISPIEL == 11
+#include "Beispiele/Templates05.cpp"
 #endif
 
 #if AKTIVE_AUFGABE == 1
