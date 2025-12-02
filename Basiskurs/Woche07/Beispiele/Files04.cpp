9b819b07c3804355f9ee02876ba7003753b9c85c
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-02 10:26:15 +0100

    Add file processing and update active example
    Enhanced `Files04.cpp` with a new `Array` class template and file handling logic to read and process words from `Datei1.txt`. Updated `main` to include file I/O operations, error handling, and word processing loops.
    
    Modified `Datei1.txt` and `Testfile01.txt` with minor text changes. Adjusted `Main.cpp` to set `AKTIVE_BEISPIEL` to 18, activating the new example.

--- Basiskurs/Woche07/Beispiele/Files04.cpp
+++ Basiskurs/Woche07/Beispiele/Files04.cpp
@@ -1,13 +1,50 @@
 ﻿
 #include <iostream>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
+template <typename T, int N>
+class Array
+{
+public:
+    string words[N];
+    int anzahl = 0;
+};
+
 int main()
 {
+    fstream file("Datei1.txt", ios::in | ios::out); // Ausgabestream (output file stream, of) in C++.
 
+    if (!file.is_open()) // file.fail()
+    {
+        cout << "Could not open the file" << endl;
+        return 1;
+    }
+
+    constexpr int m = 100;
+
+    Array<int, m> test;
+
+    while (file >> test.words[test.anzahl])
+    {
+        if (test.anzahl == 99)
+            break;
+        test.anzahl++;
+    }
+
+	//for (each) gibt das gesamte Array aus, auch wenn nichts sinnvoles mehr darin steht, es sind 100 Plätze reserviert
+    /*for (string variable : test.words)
+    {
+        std::cout << variable << std::endl;
+        file << variable << " ";
+    }*/
+
+    for (int i = 0; i < test.anzahl; ++i)
+    {
+        std::cout << test.words[i] << std::endl;
+        file << test.words[i] << " ";
+    }
 
     return 0;
 }
