18ad72967a219e5a6a172d5f6cec3497bd3b5d59
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-02 14:13:54 +0100

    Refactor vector traversal and add neighbor sum logic
    Replaced iterator-based loops with index-based loops for traversing
    the 2D vector `vect`. Introduced a new 2D vector `summenmatrix`
    initialized with zeros. Added logic to calculate the sum of
    neighboring elements for each element in `vect` with boundary
    checks. Updated output to include the value of each element
    and its calculated neighbor sum. Commented out unused code
    for alternative traversal methods.

--- Basiskurs/Woche07/Beispiele/VectorAufgabe01.cpp
+++ Basiskurs/Woche07/Beispiele/VectorAufgabe01.cpp
@@ -20,30 +20,71 @@ int main()
     vect.push_back(vect3);
 
     //oder:
-    /*std::vector<std::vector<int>> vect = 
+    std::vector<std::vector<int>> summenmatrix = 
     {
-    {1, 2, 3},
-    {4, 5, 6},
-    {7, 8, 9}
-    };*/
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
+    };
 
-    for (auto it1 = vect.begin(); it1 != vect.end(); it1++)
+   /* for (auto itZeile = vect.begin(); itZeile != vect.end(); itZeile++)
     {
-        for (auto it2 = it1->begin(); it2 != it1->end(); it2++)
+        for (auto itSpalte = itZeile->begin(); itSpalte != itZeile->end(); itSpalte++)
         {
-            std::cout << *it2 << std::endl;
+            std::cout << "Zahl: " << *itSpalte << std::endl;
+        }
+    }*/
+
+    for (int zeile = 0; zeile < vect.size(); ++zeile)
+    {
+        for (int spalte = 0; spalte < vect[zeile].size(); ++spalte)
+        {
+            std::cout << "Zahl: " << vect[zeile][spalte] << std::endl;
+
+            int summe = 0;
+
+            if (zeile > 0)
+            {
+                summe += vect[zeile - 1][spalte];
+            }
+
+            if (zeile < vect.size() - 1)
+            {
+                summe += vect[zeile + 1][spalte];
+            }
+
+            if (spalte > 0)
+            {
+                summe += vect[zeile][spalte-1];
+            }
+
+            if (spalte < vect[zeile].size() - 1)
+            {
+                summe += vect[zeile][spalte + 1];
+            }
+            
+        	summenmatrix[zeile][spalte];
+
+            std::cout << "Summe: " << summe << std::endl;
         }
     }
 
-    // Kurz & modern mit range - based for
-    for (const auto& row: vect) 
-    {
-        for (int value : row) 
-        {
-	        std::cout << value << " ";
-        }
-        std::cout << std::endl;
-    }
+    // Mit range - based for
+    //for (std::vector<int> row: vect)
+    //{
+    //    for (int value : row) 
+    //    {
+	   //     std::cout << value << " ";
+    //    }
+    //    std::cout << std::endl;
+    //}
+
+    //// oder ohe Kopie mit Referenz
+    //for (const auto& row : vect)
+    //{
+    //    for (int value : row)
+    //        std::cout << value << " ";
+    //}
 
     return 0;
 }
