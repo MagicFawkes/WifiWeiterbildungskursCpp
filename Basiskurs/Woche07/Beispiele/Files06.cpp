db62f443448ada9772f25a4ca8d20c67ddc13a72
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-02 11:53:32 +0100

    Implement queue using linked list and update input file
    Added a `Node` structure and global pointers `front` and `rear` to implement a queue using a linked list. Introduced `enque`, `deque1`, and `printqueue` functions to manage queue operations. Updated the `main()` function to read integers from `Zahlenwerte.txt`, enqueue them, and print the queue contents. Expanded `Zahlenwerte.txt` with additional numbers.

--- Basiskurs/Woche07/Beispiele/Files06.cpp
+++ Basiskurs/Woche07/Beispiele/Files06.cpp
@@ -1,6 +1,6 @@
 ﻿
 /*
- *file hast du zahlen, zahlen in queue linked list
+ *file hast du zahlen, Zahlen einlesen, zahlen in queue linked list speichern und danach ausgebenS
  */
 
 #include <fstream>
@@ -9,7 +9,64 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node* next;
+};
 
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void enque(int x)
+{
+    Node* newNode = new Node();
+    newNode->data = x;
+    newNode->next = NULL;
+
+    if (front == NULL && rear == NULL)
+    {
+        front = newNode;
+        rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void deque1() //Die Funktion existiert, aber der Fehler "deque is ambiguous" kommt NICHT, weil sie fehlt – sondern weil der Name deque mit std::deque kollidiert.
+{
+    Node* temp;
+    temp = front;
+
+    if (temp == NULL && rear == NULL)
+    {
+        cout << "Empty queue" << endl;
+        return;
+    }
+
+    front = front->next;
+    delete temp;
+}
+
+void printqueue()
+{
+    if (front == NULL && rear == NULL)
+    {
+        cout << "Empty queue" << endl;
+        return;
+    }
+
+    Node* temp = front;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
 
 int main()
 {
@@ -21,7 +78,14 @@ int main()
         return 1;
     }
 
+    int wert;
 
+    while (file >> wert)
+    {
+        enque(wert);
+    }
+
+    printqueue();
 
     return 0;
 }
