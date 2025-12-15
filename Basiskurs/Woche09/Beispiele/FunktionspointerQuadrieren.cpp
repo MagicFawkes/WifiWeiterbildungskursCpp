aadf051db119614d1e98b26d63bae2dded3d2d52
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 20:43:48 +0100

    Demonstrate squaring with pointers, functors, and lambdas
    Expanded FunktionspointerQuadrieren.cpp to show multiple ways of squaring vector elements: added function pointer and functor examples, a lambda, and updated main() to demonstrate each approach. Changed Main.cpp to activate this example by setting AKTIVE_BEISPIEL to 5.

--- Basiskurs/Woche09/Beispiele/FunktionspointerQuadrieren.cpp
+++ Basiskurs/Woche09/Beispiele/FunktionspointerQuadrieren.cpp
@@ -3,6 +3,7 @@
 */
 
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -21,9 +22,41 @@ int Generic_Function(vector<int>& values, int(*ptr_func)(int, int))     //bei Fu
     return sum;
 }
 
+void Generic_FunctionV2(vector<int>& values, void(*ptr_func)(int))
+{
+    std::for_each(values.begin(), values.end(), ptr_func);
+};
+
 int quadrieren(int x, int y)
 {
     return x * y;
+};
+
+void quadrierenV2(int x)
+{
+    cout << x * x << std::endl;
+};
+
+//Mit Functor
+struct Quadrieren 
+{
+    void operator()(int x)
+	{
+        x = x * x;
+        cout << x << std::endl;
+    }
+};
+
+
+//Mit Functor
+struct QuadrierenClass
+{
+public:
+    void operator()(int x)
+    {
+        x = x * x;
+        cout << x << std::endl;
+    }
 };
 
 int main()
@@ -38,5 +71,30 @@ int main()
     int (*function)(int, int) = quadrieren;             //bei Funktionspointern kannst du den Rückgabetyp NICHT weglassen. Er muss immer explizit angegeben werden
     //int (*function)(int, int) = &quadrieren;      //gleichwertig
     cout << Generic_Function(a, function) << endl;
+
+    //Mit Funktionspointer:
+    cout << "Mit Funktionspointer:" << std::endl;
+    Generic_FunctionV2(a, quadrierenV2);
+
+    //Direkt mit Funktion
+    cout << "Mit Funktion:" << std::endl;
+    for_each(a.begin(), a.end(), quadrierenV2);
+
+    //Variante mit Lambda:
+    cout << "Mit Lambda:" << std::endl;
+    for_each(a.begin(), a.end(), [](int& x) {
+        x *= x;
+        cout << x << std::endl;
+        });
+
+    //Direkt mit Structor Strcut
+    cout << "Mit StructorS Struct: " << std::endl;
+    std::for_each(a.begin(), a.end(), Quadrieren());
+
+    //Direkt mit Structor Class
+    cout << "Mit Structor Class: " << std::endl;
+    std::for_each(a.begin(), a.end(), QuadrierenClass());
+
+    
 }
 
