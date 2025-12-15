8cb0659356c7f3d01e6b9e55c63861942fe98e7d
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 13:41:17 +0100

    Refactor to use function pointer instead of std::function
    Replaced std::function and lambda with a function pointer for the operation in Generic_Function. Introduced a separate quadrieren function and updated main to pass it as a function pointer, simplifying the code.

--- Basiskurs/Woche09/Beispiele/FunktionspointerQuadrieren.cpp
+++ Basiskurs/Woche09/Beispiele/FunktionspointerQuadrieren.cpp
@@ -2,35 +2,34 @@
 
 */
 
+
 #include <iostream>
 #include <vector>
-#include <functional>
 
 using namespace std;
 
-int Generic_Function(vector<int> values, function<int(int, int)> func) 
+// Mit Funktionspointer als Übergabe
+int Generic_Function(vector<int>& values, int(*ptr_func)(int, int))
 {
     int sum = 1;
 
-    for (int value : values) 
+    for (int value : values)
     {
-        sum = func(sum, value);
+        sum = ptr_func(sum, value);
     }
     return sum;
 }
+
+int quadrieren(int x, int y)
+{
+    return x * y;
+};
 
 int main()
 {
     vector<int> a = { 1,2,3,4,5 };
 
-    int summ = 0;
-
-    auto lambda = [](int x, int y)
-    {
-        return x * y;
-    };
-
-    int wert = Generic_Function(a, lambda);
+    int wert = Generic_Function(a, quadrieren);
 
     cout << wert << endl;
 }
