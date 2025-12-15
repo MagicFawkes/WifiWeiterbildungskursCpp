a1d96f75751af100d58108f6bf0d61f791f96660
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 14:36:35 +0100

    Add functor and lambda examples with Person sorting
    Introduced Number and Person functor classes, including a salary comparison functor (VergleicheGehalt). Updated main() to demonstrate functor usage, sorting a vector of Person objects by salary, and printing results using a lambda. Removed unused <functional> and added <algorithm> for sorting and iteration.

--- Basiskurs/Woche09/Beispiele/Functor01.cpp
+++ Basiskurs/Woche09/Beispiele/Functor01.cpp
@@ -1,26 +1,66 @@
 ﻿/*
-Sinn & Unterschied
-🔹 Lambda – modern & flexibel
 
-Was ist der Sinn?
-Kleine Funktionen direkt am Einsatzort
-Können Zustand (Captures) mitnehmen
-Typ wird automatisch abgeleitet
-Perfekt für STL, Callbacks, Templates
-
-Kurzempfehlung (wichtig!)
-👉 Nimm Lambdas als Standard.
-👉 Funktionspointer nur, wenn du sie wirklich brauchst.
 */
 
 #include <iostream>
 #include <vector>
-#include <functional>
+#include <algorithm>
 
 using namespace std;
 
+class Number {
+private:
+    int n;
+public:
+    int operator()(int input)
+    {
+        n = input;
+        return n;
+    }
+    int operator()(int input1, int input2)
+    {
+        return n + input1 + input2;
+    }
+};
+
+struct Person {
+    std::string name;
+    int alter;
+    float gehalt;
+
+    bool operator<(const Person& a) const {
+        return alter < a.alter;
+    }
+
+    bool operator()(const Person& a, const Person& b) const {
+        return a.alter < b.alter;
+    }
+
+
+};
+
+struct VergleicheGehalt {
+    bool operator()(const Person& a, const Person& b) const {
+        return a.gehalt < b.gehalt;
+    }
+};
+
 int main()
 {
-   
-}
+    Number n;
+    n(10);
 
+    n(10, 20);
+
+    std::vector<Person> personen = {
+        {"Anna",32,2550},
+        {"bob",25,3500},
+        {"Clara",40,2900}
+    };
+
+    std::sort(personen.begin(), personen.end(), VergleicheGehalt());
+
+    std::for_each(personen.begin(), personen.end(), [](Person p) {
+        cout << p.gehalt << endl;
+        });
+}
