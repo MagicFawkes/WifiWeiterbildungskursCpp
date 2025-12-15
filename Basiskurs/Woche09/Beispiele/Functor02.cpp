e804bad027fad686b051918dfbea3ea2ec132761
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 15:56:26 +0100

    Rewrite Functor02.cpp to demonstrate STL functors
    Completely overhaul Functor02.cpp to introduce and demonstrate custom functor classes, including Number, Person, VergleicheGehalt, and an abstract funktor base class with a derived stl class. The new main() uses these functors with STL algorithms such as std::for_each and std::sort. Also, update Main.cpp to set AKTIVE_BEISPIEL to 7, enabling the new example.

--- Basiskurs/Woche09/Beispiele/Functor02.cpp
+++ Basiskurs/Woche09/Beispiele/Functor02.cpp
@@ -2,12 +2,84 @@
  
 */
 
+#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+class Number
+{
+private:
+    int n;
+public:
+    int operator()(int input)
+    {
+        n = input;
+        return n;
+    }
+    int operator()(int input1, int input2)
+    {
+        return n + input1 + input2;
+    }
+};
+
+struct Person {
+    std::string name;
+    int alter;
+    float gehalt;
+
+    bool operator<(const Person& a) const {
+        return alter < a.alter;
+    }
+
+    bool operator()(const Person& a, const Person& b) const {
+        return a.alter < b.alter;
+    }
+
+
+};
+
+struct VergleicheGehalt {
+    bool operator()(const Person& a, const Person& b) const {
+        return a.gehalt < b.gehalt;
+    }
+};
+
+
+class funktor {
+public:
+    virtual void operator()(int& x) = 0;
+    virtual bool operator()(int x, int y) = 0;
+
+};
+
+class stl :public funktor
+{
+public:
+    void operator()(int& x) {
+        x = x * x;
+        return;
+    } //remove
+    bool operator()(int x, int y) {
+        return x < y;
+    }
+};
 
 
 int main()
 {
+    vector<int> a = { 2,1,10,5, 6 };
 
+    /*std::remove_if(a.begin(), a.end(), stl());*/
+
+    std::for_each(a.begin(), a.end(), stl());
+
+
+    std::sort(a.begin(), a.end(), stl());
+
+    std::for_each(a.begin(), a.end(), [](int x) {
+        cout << x << endl;
+        });
 }
+
