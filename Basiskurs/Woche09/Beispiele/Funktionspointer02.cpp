2cff51685dbf0956fa6c055a90c82dd2db01cb13
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 11:31:18 +0100

    Refactor: focus on function pointers for arithmetic ops
    Removed all sorting-related code and refactored the example to demonstrate function pointers for addition and multiplication. Added user input to select the operation and numbers, and display the result. Cleaned up unused functions and streamlined the code for clarity.

--- Basiskurs/Woche09/Beispiele/Funktionspointer02.cpp
+++ Basiskurs/Woche09/Beispiele/Funktionspointer02.cpp
@@ -2,113 +2,50 @@
 	Funktionspointer Beispiel
 */
 
-
 #include <iostream>
-#include <vector>
 #include<fstream>
 
 using namespace std;
-
-
-void function(int x) {
-    cout << x << endl;
-}
 
 int add(int a, int b) {
     return a + b;
 }
 
-int sub(int a, int b) {
-    return a - b;
+int mult(int a, int b) {
+    return a * b;
 }
-
-bool Aufsteigend(int a, int b) {
-    return a > b;
-}
-
-bool Absteigend(int a, int b) {
-    return a < b;
-}
-
-void bubbleSortAufsteigend(vector<int>& arr) {
-
-    for (int i = 0; i < arr.size(); i++) {
-        for (int k = 0; k < arr.size() - 1 - i; k++) {
-            if (Aufsteigend(arr[k], arr[k + 1])) {
-                int temp = 0;
-                arr[k] = arr[k + 1];
-                arr[k + 1] = temp;
-            }
-        }
-    }
-
-}
-
-
-void bubbleSortAbsteigend(vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        for (int k = 0; k < arr.size() - 1 - i; k++) {
-            if (Absteigend(arr[k], arr[k + 1]))
-            {
-                int temp = 0;
-                arr[k] = arr[k + 1];
-                arr[k + 1] = temp;
-            }
-        }
-    }
-}
-
-
-void bubblesort(vector<int>& arr, bool(*ptr)(int, int))
-{
-    for (int i = 0; i < arr.size(); i++) {
-        for (int k = 0; k < arr.size() - 1 - i; k++) {
-            if (ptr(arr[k], arr[k + 1]))
-            {
-                int temp = 0;
-                arr[k] = arr[k + 1];
-                arr[k + 1] = temp;
-            }
-        }
-    }
-
-}
-
-int (*select_operation())(int, int) {
-    //Code...//
-    return add;
-}
-
 
 int main()
 {
-    int c = 10;
-    int* ptr = &c;
-    ptr = &c;
+    int auswahl, a, b;
 
-    void(*function_pointer)(int) = &function;
-    //void(*function_pointer)(int);
-    //function_pointer = function;
-    //function_pointer = &function;
+    cout << "Bitte Rechenoperation auswwaehlen Addieren (1), Multiplizieren (2): ";
+    cin >> auswahl;
+    cout << "Bitte erste Zahl eingeben: ";
+    cin >> a;
+    cout << "Bitte zweite Zahl eingeben: ";
+    cin >> b;
+    
+	int (*ptr_func)(int, int) = nullptr;
 
-    (*function_pointer)(10);
+    switch (auswahl)
+    {
+    case 1:
+        ptr_func = add;
+        break;
+    case 2:
+        ptr_func = mult;
+        break;
+	default: 
+        ptr_func = add;
+    }
 
-    int(*ptr_func)(int, int);
+    if (ptr_func == nullptr)
+    {
+        return 0;
+    }
 
-    ptr_func = add;
-
-    int result = (*ptr_func)(1, 2);
-
-    cout << result << endl;
-
-    int (*a[])(int, int) = { add,sub };
-
-    a[0] = &sub;
-    int summe = (a[0])(10, 5);
-
-    vector<int> arr = { 1,2,4,9,5,0 };
-
-    bubblesort(arr, Aufsteigend);
+    cout << "Ergebnis: " << (*ptr_func)(a, b) << endl;
 }
 
 
