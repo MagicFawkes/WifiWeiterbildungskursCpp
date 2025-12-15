c5d737019388e16829767ec4da76678a7d0ed3be
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-15 13:07:25 +0100

    Add vector sum calculation using lambda and for_each
    Added a vector of integers and used std::for_each with a lambda to compute and print the sum of its elements.

--- Basiskurs/Woche09/Beispiele/Lambda02.cpp
+++ Basiskurs/Woche09/Beispiele/Lambda02.cpp
@@ -11,7 +11,15 @@ using namespace std;
 
 int main()
 {
+    vector<int> a = { 1,2,3,4,5 };
 
+    int summ = 0;
 
+    std::for_each(a.begin(), a.end(), [&summ](int x)
+        {
+            summ = summ + x;
+        });
+
+    cout << summ << endl;
 }
 
