536db76b1c31823bddd3789235d1f1914098f98b
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-16 09:26:08 +0100

    Refactor Person and bubblesort for flexible comparison
    Added operator> to Person and clarified comparator usage with comments. Introduced a generic bubblesort accepting custom comparators, and replaced the old function pointer version. Updated main to demonstrate both comparator and operator< sorting. Changed output to display age instead of salary.

--- Basiskurs/Woche09/Beispiele/FunctorSortGeneric.cpp
+++ Basiskurs/Woche09/Beispiele/FunctorSortGeneric.cpp
@@ -15,25 +15,44 @@ struct Person
         return alter < a.alter;
     }
 
+    bool operator>(const Person& a) const
+    {
+        return alter > a.alter;
+    }
+
     bool operator()(const Person& a, const Person& b) const
     {
         return a.alter < b.alter;
     }
 };
 
-template<typename T>
-void bubblesort(std::vector<T>& arr, bool(*ptr)(T, T))
+/*
+ *dann passiert Folgendes:
+
+T ist Person
+T comp; erzeugt ein Person-Objekt
+comp(a, b) ruft Person::operator() auf
+➡️ operator< kommt hier NIE zum Einsatz, weil du ihn gar nicht verwendest.
+// operator() wird aufgerufen, weil ein Person-Objekt als Funktionsobjekt (comp(a,b)) verwendet wird und nicht der <-Operator (a < b).
+*/
+
+template<typename T, typename Compare>
+void bubblesort(std::vector<T>& v, Compare comp)
 {
-    for (int i = 0; i < arr.size(); i++) {
-        for (int k = 0; k < arr.size() - 1 - i; k++) {
-            if (ptr(arr[k], arr[k + 1]))
-            {
-                int temp = 0;
-                arr[k] = arr[k + 1];
-                arr[k + 1] = temp;
-            }
-        }
-    }
+    for (size_t i = 0; i < v.size(); ++i)
+        for (size_t j = 0; j + 1 < v.size() - i; ++j)
+            if (comp(v[j + 1], v[j]))       //es wird wie eine Funktion aufgerufen comp(a,b) daher wird Operator () verwendet
+                std::swap(v[j], v[j + 1]);
+}
+
+//Funktion überladen damit der < oder > operator inder Klasse Person auch verwendet werden kann
+template<typename T>
+void bubblesort(std::vector<T>& v)
+{
+    for (size_t i = 0; i < v.size(); ++i)
+        for (size_t j = 0; j + 1 < v.size() - i; ++j)
+            if (v[j + 1] < v[j])       //es wird wie eine Funktion aufgerufen comp(a,b) daher wird Operator <()> verwendet
+                std::swap(v[j], v[j + 1]);
 }
 
 int main()
@@ -44,10 +63,14 @@ int main()
         {"Clara",40,2900}
     };
 
+    // Operator () wird verwendet, da intern in Bubble Sort das Objekt als Funktion aufgerufen wird also comp()
     bubblesort(personen, Person());
+    
+    // Operator < oder > wird verwendet, Template überladen
+	bubblesort(personen);
 
     // Mit Lambda Funktion alles durchlaufen
     std::for_each(personen.begin(), personen.end(), [](const Person& p)->void {
-	    std::cout << p.gehalt << std::endl;
+	    std::cout << p.alter << std::endl;
         });
 }
