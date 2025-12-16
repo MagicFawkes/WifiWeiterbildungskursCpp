10ffc931a33ce21ac0e45dfec69a8c4a39bce3ce
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-16 10:02:36 +0100

    Add examples and comments on C++ lambda usage and const
    Expanded main() with new lambda examples for double division, including function pointer and std::function variants. Added detailed comments explaining lambda captures, const correctness, and mutable usage. Adjusted output to demonstrate new cases and improved code clarity for educational purposes.

--- Basiskurs/Woche09/Beispiele/LambdaDoubleDiv.cpp
+++ Basiskurs/Woche09/Beispiele/LambdaDoubleDiv.cpp
@@ -23,10 +23,13 @@ int main()
     double a = 254;
     double b = 12;
 
+    //Mit Funktionspointer capture ist hier nicht möglich
     double (*lambda1)(double, double) = [](double a, double b)->double
         {
             return a / b;
         };
+
+    std::cout << lambda1(a, b) << std::endl;
 
     //auch Möglich, Capture wäre dann hier okay
     std::function<double(double, double)> lambda2 = [](double a, double b)->double
@@ -34,8 +37,17 @@ int main()
             return a / b;
         };
 
-    std::cout << lambda1(a, b) << std::endl;
     std::cout << lambda2(a, b) << std::endl;
+
+    //soll Intern keine Wertänderung möglch seinn dann const am ebde dazu schreiben:
+    // const beim Rückgabewert ist hier wirkungslos, da der Wert per Kopie zurückgegeben wird
+    std::function<double(double, double)> lambda6 = [](const double a, const double b)->const double        //const macht hier keinen sinn, nur wenn als Referenz oder Zeiger Rückgabewert gegeben wird
+        {                                                                                                  // ->double const geht auch
+            //a++; Wert lässt sich nicht ändern wegen const
+            return a / b;
+        };
+
+    std::cout << lambda6(a, b) << std::endl;
 
     //weitere Varianten mit Capture, Wert verändern darin daher Adressoperator &c (ohne Adressoperator wäre :
     int c = 22;
@@ -49,9 +61,10 @@ int main()
 
     //weitere Varianten mit Capture, Wert verändern ohne Adressoperator dafür Kopie und Mutable, damit er verändert werden kann
     int d = 22;
-    std::function<double(double, double)> lambda4 = [d](double a, double b) mutable ->double
+    std::function<double(double, double)> lambda4 = [d](double a, double b) mutable ->double    //mutable ->double const würde sich widersprechen und macht keinen Sinn
         {
             d++;
+            a++;
             return (a / b) + d;
         };
 
