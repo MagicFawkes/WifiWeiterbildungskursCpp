a416d7b7dac241b97f24f6124a420bb40ac35279
MagicFawkes <107568517+MagicFawkes@users.noreply.github.com>
2025-12-21 09:10:25 +0100

    Add filter_and_transform template and update main logic
    Introduced a new filter_and_transform template function in Aufgabe01.cpp, along with supporting includes. Updated main to use this function with example lambdas and a sample vector. Switched Main.cpp to run Aufgabe01.cpp by setting AKTIVE_AUFGABE to 1 and disabling example files. Note: filter_and_transform implementation is currently incomplete.

--- Basiskurs/Woche09/Aufgaben/Aufgabe01.cpp
+++ Basiskurs/Woche09/Aufgaben/Aufgabe01.cpp
@@ -13,7 +13,41 @@ TransformFunc transform
 */
 
 
+#include <functional>
+#include <iostream>
+#include <vector>
+
+template<typename T, typename FilterFunc, typename TransformFunc>
+std::vector<T> filter_and_transform(const std::vector<T>& input, FilterFunc filter, TransformFunc transform)
+{
+    for (int x : v) {
+        f(x);
+    }
+
+    return ;
+}
+
+
 int main()
 {
+    std::vector<int> werteListe = { 2,1,10,5, 6 };
+
+    std::function<double(double, double)> lambdaFilter = [](double a, double b)->double
+        {
+            return a / b;
+        };
+
+    std::function<double(double, double)> lambdaTransform = [](double a, double b)->double
+        {
+            return a / b;
+        };
+
+    std::vector<int> filteredValues = filter_and_transform(werteListe, lambdaFilter, lambdaTransform);
+
+    for (int werte : filteredValues)
+    {
+        std::cout << werte << std::endl;
+    }
+
     return 0;
 }
