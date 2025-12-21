2178ec71fd3ff345c523b79b7468ed5aa5f4bb0d
MagicFawkes <107568517+MagicFawkes@users.noreply.github.com>
2025-12-21 09:35:16 +0100

    Update filter_and_transform to filter and transform in one step
    Refactored filter_and_transform to apply transformation after filtering, using std::transform. Made the function generic by changing intermediate vector type to std::vector<T>. Updated main() to use a larger input vector, changed filter to select values >5, and changed transform to square integers instead of dividing doubles.

--- Basiskurs/Woche09/Aufgaben/Aufgabe01.cpp
+++ Basiskurs/Woche09/Aufgaben/Aufgabe01.cpp
@@ -20,25 +20,26 @@ TransformFunc transform
 template<typename T, typename FilterFunc, typename TransformFunc>
 std::vector<T> filter_and_transform(const std::vector<T>& input, FilterFunc filter, TransformFunc transform)
 {
-    std::vector<int> valuesFiltered;
-	std::copy_if(input.begin(), input.end(), std::back_inserter(valuesFiltered), filter);
+    std::vector<T> valuesFilteredAndTransformed;
+	std::copy_if(input.begin(), input.end(), std::back_inserter(valuesFilteredAndTransformed), filter);
+    std::transform(valuesFilteredAndTransformed.begin(), valuesFilteredAndTransformed.end(), valuesFilteredAndTransformed.begin(), transform);
 
-    return valuesFiltered;
+    return valuesFilteredAndTransformed;
 }
 
 int main()
 {
-    std::vector<int> werteListe = {2,1,10,5,6};
+    std::vector<int> werteListe = {2,1,10,5,6,24,1,53};
 
     std::function<bool(int a)> lambdaFilter = [](int value)->bool
         {
-            return value > 10;
+            return value > 5;
         };
 
-    std::function<double(double, double)> lambdaTransform = [](double a, double b)->double
-        {
-            return a / b;
-        };
+    std::function<int(int)> lambdaTransform = [](int x) ->int
+    {
+	    return x * x;
+    };
 
     std::vector<int> filteredValues = filter_and_transform(werteListe, lambdaFilter, lambdaTransform);
 
