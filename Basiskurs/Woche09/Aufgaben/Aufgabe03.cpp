5da184c8420169f7389a3489c07f2fcb6e68c1e3
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-21 17:34:05 +0100

    Implement generic analyze_data with filter, transform, reduce
    Added Aufgabe03.cpp with a templated analyze_data function that generically filters, transforms, and reduces a vector using provided functors or lambdas. Included example functors for filtering (>5), squaring, and summing. Updated Main.cpp to activate Aufgabe03 as the current task.

--- Basiskurs/Woche09/Aufgaben/Aufgabe03.cpp
+++ Basiskurs/Woche09/Aufgaben/Aufgabe03.cpp
@@ -31,11 +31,59 @@ Anforderungen
 5. Gib das Ergebnis der Aggregation zurück6. Filtere alle Elemente, die die Filterbedingung erfüllen
 7. Transformiere die gefilterten Elemente
 8. Aggregiere (summiere) das Ergebnis zu einem einzelnen Wert
-
 */
 
 
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+template<typename T>
+struct LambdaFilterFunctor
+{
+    bool operator()(const T& a) const
+    {
+        return a > 5;
+    }
+};
+
+template<typename T>
+struct LambdaTransformFunctor
+{
+    T operator()(const T& x) const
+    {
+        return x * x;
+    }
+};
+
+template<typename T>
+struct LambdaReduceFunctor
+{
+    T operator()(const T& a, const T& b) const
+    {
+        return a + b;
+    }
+};
+
+template<typename T, typename Filter, typename Transform, typename Reduce>
+T analyze_data(const std::vector<T>& data, Filter filter, Transform transform, Reduce reduce, T init)
+{
+    std::vector<T> valuesFilteredAndTransformed;
+    std::copy_if(data.begin(), data.end(), std::back_inserter(valuesFilteredAndTransformed), filter);
+    std::transform(valuesFilteredAndTransformed.begin(), valuesFilteredAndTransformed.end(), valuesFilteredAndTransformed.begin(), transform);
+
+    return std::accumulate(valuesFilteredAndTransformed.begin(), valuesFilteredAndTransformed.end(), init, reduce);
+}
+
 int main()
 {
+    std::vector<int> werteListe = { 2,1,10,5,6,24,1,53 };
+
+    int filteredValues = analyze_data(werteListe, LambdaFilterFunctor<int>(), LambdaTransformFunctor<int>(), LambdaReduceFunctor<int>(), 0);
+
+    std::cout << filteredValues << std::endl;
+
     return 0;
 }
