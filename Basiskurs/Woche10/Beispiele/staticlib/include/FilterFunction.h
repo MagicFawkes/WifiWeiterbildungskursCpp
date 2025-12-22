582c8bdf8c5a158d7804158bcc85014c635602d7
Wiesinger Manuel <107568517+MagicFawkes@users.noreply.github.com>
2025-12-22 11:14:52 +0100

    Add filter_and_transform template and extern instantiation
    Declared filter_and_transform template in FilterFunction.h with
    support for custom filter and transform functions. Added extern
    template instantiation for int and std::function types to optimize
    compilation. Updated FilterFunction.lib with compiled instantiation.

--- Basiskurs/Woche10/Beispiele/staticlib/include/FilterFunction.h
+++ Basiskurs/Woche10/Beispiele/staticlib/include/FilterFunction.h
@@ -1,9 +1,18 @@
-#pragma once
+﻿#pragma once
 #include <vector>
+#include <functional>
 
 template<typename T, typename FilterFunc, typename TransformFunc>
 std::vector<T> filter_and_transform(
     const std::vector<T>& input,
     FilterFunc filter,
     TransformFunc transform
+);
+
+// 🔴 WICHTIG: Ankündigung der Instanz
+extern template std::vector<int>
+filter_and_transform<int, std::function<bool(int)>, std::function<int(int)>>(
+    const std::vector<int>&,
+    std::function<bool(int)>,
+    std::function<int(int)>
 );
